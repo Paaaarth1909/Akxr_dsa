5f1c2a4bb517967cbd59024eca1a8a37a54c901b
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-17 22:03:25 +0530

    COMMIT

--- GFG/Basic/Linked_List_insertion_at_end.java
+++ GFG/Basic/Linked_List_insertion_at_end.java
@@ -17,3 +17,28 @@ Constraints:
 0 ≤ node->data , x ≤ 103
 
  */
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+        Node newNode = new Node(x);
+        if (head == null) {
+            return newNode;
+        }
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+        return head;
+    }
+}
