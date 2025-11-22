3ba4c5ca9cbec54c3f56d4c807c99f9e3cd90d35
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-22 13:16:38 +0530

    COMMIT

--- LEETCODE/EASY/Keep_multiplying_found_values_by_two_2154.java
+++ LEETCODE/EASY/Keep_multiplying_found_values_by_two_2154.java
@@ -32,3 +32,14 @@ Constraints:
 1 <= nums[i], original <= 1000
  
 */
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == original) {
+                i = -1;
+                original *= 2;
+            }
+        }
+        return original;
+    }
+}
