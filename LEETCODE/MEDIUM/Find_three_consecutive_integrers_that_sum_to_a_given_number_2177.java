b97dc9401bd7efa8dc2267f7fb0cb867353daba9
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-29 23:24:09 +0530

    COMMIT

--- LEETCODE/MEDIUM/Find_three_consecutive_integrers_that_sum_to_a_given_number_2177.java
+++ LEETCODE/MEDIUM/Find_three_consecutive_integrers_that_sum_to_a_given_number_2177.java
@@ -19,3 +19,12 @@ Constraints:
 
 0 <= num <= 1015
 */
+class Solution {
+    public long[] sumOfThree(long num) {
+        long x = num / 3;
+        if (x - 1 + x + x + 1 == num) {
+            return new long[]{x - 1, x, x + 1};
+        }
+        return new long[0];
+    }
+}
