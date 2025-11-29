f86b57b63d1faf354b715d54a896b27aae3233ce
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-29 23:26:46 +0530

    COMMIT

--- GFG/Easy/Arrau_Duplicates.java
+++ GFG/Easy/Arrau_Duplicates.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given an array arr[] of size n, containing elements from the range 1 to n, and each element appears at most twice, return an array of all the integers that appears twice.
 
 Note: You can return the elements in any order but the driver code will print them in sorted order.
