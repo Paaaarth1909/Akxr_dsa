fc051dca7dd13fe411b051913a95163ab0366e85
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-05 21:09:05 +0530

    COMMIT

--- LEETCODE/MEDIUM/Valid_sudoku_36.java
+++ LEETCODE/MEDIUM/Valid_sudoku_36.java
@@ -45,3 +45,23 @@ board.length == 9
 board[i].length == 9
 board[i][j] is a digit 1-9 or '.'.
 */
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                int num = board[i][j] - '1';
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                if (row[i][num] || col[j][num] || box[boxIndex][num]) return false;
+                row[i][num] = true;
+                col[j][num] = true;
+                box[boxIndex][num] = true;
+            }
+        }
+        return true;
+    }
+}
