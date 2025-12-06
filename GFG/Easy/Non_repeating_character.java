d0107309fa1e80adc051821ee8ffcc71e6f52554
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-06 16:03:08 +0530

    COMMIT

--- GFG/Easy/Non_repeating_character.java
+++ GFG/Easy/Non_repeating_character.java
@@ -15,3 +15,18 @@ Constraints:
 1 ≤ s.size() ≤ 105
 
 */
+class Solution {
+    public char nonRepeatingChar(String s) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (char c : s.toCharArray()) {
+            if (freq[c - 'a'] == 1) return c;
+        }
+
+        return '$';
+    }
+}
