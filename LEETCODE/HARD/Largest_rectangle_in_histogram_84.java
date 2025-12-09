a47a543147dcc0915ae43a9bfbee625d65b4145f
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-09 23:48:33 +0530

    COMMIT

--- LEETCODE/HARD/Largest_rectangle_in_histogram_84.java
+++ LEETCODE/HARD/Largest_rectangle_in_histogram_84.java
@@ -1,3 +1,5 @@
+package LEETCODE.HARD;
+
 /* Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
 
  
