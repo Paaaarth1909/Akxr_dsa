ea7005c6134ad321d3670ab836f3c85e186f9c45
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-11 21:01:09 +0530

    COMMIT

--- GFG/Easy/Count_Digits.java
+++ GFG/Easy/Count_Digits.java
@@ -17,3 +17,21 @@ Explanation: 2 and 3, none of them divide 23 evenly.
 Constraints:
 1<= n <=105 
 */
+class Solution {
+    static int evenlyDivides(int n) {
+        int num = n;
+        int count = 0;
+        
+        while (num > 0) {
+            int digit = num % 10;
+            
+            if (digit != 0 && n % digit == 0) {
+                count++;
+            }
+            
+            num /= 10;
+        }
+        
+        return count;
+    }
+}
