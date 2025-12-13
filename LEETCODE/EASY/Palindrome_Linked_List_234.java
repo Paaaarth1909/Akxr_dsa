70eae68a4b227a96814140425d946aba27a1b943
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-13 23:30:32 +0530

    COMMIT

--- LEETCODE/EASY/Palindrome_Linked_List_234.java
+++ LEETCODE/EASY/Palindrome_Linked_List_234.java
@@ -25,11 +25,11 @@ The number of nodes in the list is in the range [1, 105].
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
  */
 import java.util.*;
 class Solution {
