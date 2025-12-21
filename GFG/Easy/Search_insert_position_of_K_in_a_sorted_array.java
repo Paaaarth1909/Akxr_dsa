f80f795d76d48c2b9027bd132baeac97be88e543
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-21 12:02:11 +0530

    COMMIT

--- GFG/Easy/Search_insert_position_of_K_in_a_sorted_array.java
+++ GFG/Easy/Search_insert_position_of_K_in_a_sorted_array.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given a sorted array arr[] (0-index based) of distinct integers and an integer k, find the index of k if it is present in the arr[]. If not, return the index where k should be inserted to maintain the sorted order.
 
 Examples :
