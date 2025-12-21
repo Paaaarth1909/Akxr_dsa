019846e813ce778ec087f3b3e9a8da0d5d6f4a4d
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-21 11:59:02 +0530

    COMMIT

--- LEETCODE/MEDIUM/Reorder_list_143.java
+++ LEETCODE/MEDIUM/Reorder_list_143.java
@@ -25,3 +25,33 @@ Constraints:
 The number of nodes in the list is in the range [1, 5 * 104].
 1 <= Node.val <= 1000
 */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null, curr = slow.next;
+        slow.next = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode first = head, second = prev;
+        while (second != null) {
+            ListNode t1 = first.next;
+            ListNode t2 = second.next;
+            first.next = second;
+            second.next = t1;
+            first = t1;
+            second = t2;
+        }
+    }
+}
