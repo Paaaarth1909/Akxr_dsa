bd54b906211a7894c1b599e189b28591a573b2c4
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-23 08:24:55 +0530

    COMMIT

--- GFG/Medium/Add_binary_strings.java
+++ GFG/Medium/Add_binary_strings.java
@@ -17,3 +17,30 @@ Constraints:
 1 ≤s1.size(), s2.size()≤ 106
 
 */
+class Solution {
+    public String addBinary(String s1, String s2) {
+        int i = s1.length() - 1;
+        int j = s2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            
+            if (i >= 0) sum += s1.charAt(i--) - '0';
+            if (j >= 0) sum += s2.charAt(j--) - '0';
+            
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        
+        // reverse since we built result from LSB → MSB
+        String res = sb.reverse().toString();
+        
+        // remove leading zeros
+        int idx = 0;
+        while (idx < res.length() - 1 && res.charAt(idx) == '0') idx++;
+        
+        return res.substring(idx);
+    }
+}
