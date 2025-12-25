c1b50af1525ab6e4f7e0d7a227a32b4a4ab8c108
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-25 12:20:28 +0530

    COMMIT

--- GFG/Medium/Remove_loop_in_linked_list.java
+++ GFG/Medium/Remove_loop_in_linked_list.java
@@ -21,3 +21,34 @@ Constraints:
 1 ≤ size of linked list ≤ 105
 
 */
+class Solution {
+    public static void removeLoop(Node head) {
+        if (head == null || head.next == null) return;
+
+        Node slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) break;
+        }
+
+        if (slow != fast) return;
+
+        slow = head;
+
+        if (slow == fast) {
+            while (fast.next != slow) {
+                fast = fast.next;
+            }
+        } else {
+            while (slow.next != fast.next) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+
+        fast.next = null;
+    }
+}
