a13e18f60e11edc99be98e34ea6815723b6ceaeb
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-26 08:39:21 +0530

    COMMIT

--- LEETCODE/MEDIUM/Longest_repeating_character_replacement_424.java
+++ LEETCODE/MEDIUM/Longest_repeating_character_replacement_424.java
@@ -24,3 +24,23 @@ Constraints:
 s consists of only uppercase English letters.
 0 <= k <= s.length
 */
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int l = 0, maxFreq = 0, ans = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            int idx = s.charAt(r) - 'A';
+            freq[idx]++;
+            maxFreq = Math.max(maxFreq, freq[idx]);
+
+            while (r - l + 1 - maxFreq > k) {
+                freq[s.charAt(l) - 'A']--;
+                l++;
+            }
+
+            ans = Math.max(ans, r - l + 1);
+        }
+        return ans;
+    }
+}
