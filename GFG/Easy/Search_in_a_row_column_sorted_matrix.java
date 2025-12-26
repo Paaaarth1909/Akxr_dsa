341ef4e535850c4fed1129b09e9cbd1a144c82f2
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-26 08:41:15 +0530

    COMMIT

--- GFG/Easy/Search_in_a_row_column_sorted_matrix.java
+++ GFG/Easy/Search_in_a_row_column_sorted_matrix.java
@@ -17,3 +17,39 @@ Constraints:
 1<= x <= 109
 
 */
+class Solution {
+    public static boolean matSearch(int mat[][], int x) {
+        int n =mat.length , m=mat[0].length;
+        int i=0 , j=m-1;
+        while ( i<n && j>=0){
+            if ( x>mat[i][j]){
+                i++;
+            }
+            else if ( x<mat[i][j]){
+                j--;
+            }
+            else {
+                return true;
+            }
+            
+        
+    }
+    return false;
+    
+    
+}
+public static void main(String[] args){
+    int [][] mat= { 
+        {3,30,38},
+        {20,52,54},
+        {35,60,69}
+                    
+};
+int x=35;
+if ( matSearch(mat,x))
+System.out.println("true");
+else 
+System.out.println("false");
+}
+
+}
