8c15e960bfd34d5a22136c34df7c8cbeb9ce9414
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-29 14:05:37 +0530

    COMMIT

--- GFG/Easy/Missing_and_repeating.java
+++ GFG/Easy/Missing_and_repeating.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given an unsorted array arr[] of size n, containing elements from the range 1 to n, it is known that one number in this range is missing, and another number occurs twice in the array, find both the duplicate number and the missing number.
 
 Examples:
