0d832299f179803f64c373fd74515ea4705d4c8e
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-29 14:02:31 +0530

    COMMIT

--- LEETCODE/EASY/Kth_missing_positive_number_1539.java
+++ LEETCODE/EASY/Kth_missing_positive_number_1539.java
@@ -30,3 +30,15 @@ Could you solve this problem in less than O(n) complexity?
 
  
 */
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            int missing = arr[m] - (m + 1);
+            if (missing < k) l = m + 1;
+            else r = m - 1;
+        }
+        return l + k;
+    }
+}
