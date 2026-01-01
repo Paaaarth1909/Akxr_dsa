aeae78d4ba2537d2e1bed7deb4e2c548909dbcce
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-01 17:15:08 +0530

    COMMIT

--- GFG/Medium/Agressive_cows.java
+++ GFG/Medium/Agressive_cows.java
@@ -24,3 +24,40 @@ Constraints:
 2 ≤ k ≤ stalls.size()
 
 */
+class Solution {
+    public int aggressiveCows(int[] stalls, int k) {
+        Arrays.sort(stalls);
+
+        int low = 0;
+        int high = stalls[stalls.length - 1] - stalls[0];
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canPlace(stalls, k, mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean canPlace(int[] stalls, int k, int dist) {
+        int count = 1;
+        int lastPos = stalls[0];
+
+        for (int i = 1; i < stalls.length; i++) {
+            if (stalls[i] - lastPos >= dist) {
+                count++;
+                lastPos = stalls[i];
+            }
+            if (count >= k) return true;
+        }
+
+        return false;
+    }
+}
