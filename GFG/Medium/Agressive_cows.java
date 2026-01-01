d18f627aa34b49281ea8d599992b92cc8ebc9e1e
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-01 17:15:33 +0530

    COMMIT

--- GFG/Medium/Agressive_cows.java
+++ GFG/Medium/Agressive_cows.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* You are given an array with unique elements of stalls[], which denote the positions of stalls. You are also given an integer k which denotes the number of aggressive cows. The task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible.
 
 Examples:
