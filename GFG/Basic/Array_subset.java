a46a7dbdffb594c81c7a78ed6e1f30b32a6c66a9
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-05 15:26:21 +0530

    COMMIT

--- GFG/Basic/Array_subset.java
+++ GFG/Basic/Array_subset.java
@@ -16,3 +16,23 @@ Constraints:
 1 <= a[i], b[j] <= 106
 
 */
+import java.util.*;
+
+class Solution {
+    public boolean isSubset(int a[], int b[]) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int x : a) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        }
+
+        for (int x : b) {
+            if (!freq.containsKey(x) || freq.get(x) == 0) {
+                return false;
+            }
+            freq.put(x, freq.get(x) - 1);
+        }
+
+        return true;
+    }
+}
