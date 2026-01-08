61dbc611a083389767abf16787b807e272d1110e
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-09 04:50:43 +0530

    COMMIT

--- LEETCODE/EASY/Maximum_nesting_depth_of_the_parentheses_1614.java
+++ LEETCODE/EASY/Maximum_nesting_depth_of_the_parentheses_1614.java
@@ -37,3 +37,17 @@ s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
 It is guaranteed that parentheses expression s is a VPS.
  
 */
+class Solution {
+    public int maxDepth(String s) {
+        int depth = 0, max = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                depth++;
+                max = Math.max(max, depth);
+            } else if (c == ')') {
+                depth--;
+            }
+        }
+        return max;
+    }
+}
