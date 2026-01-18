df7d8787faeb3e72f1c8f12e0f1661feff9adbda
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-18 23:57:24 +0530

    COMMIT

--- GFG/Easy/Power_of_2.java
+++ GFG/Easy/Power_of_2.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given a non-negative integer n. You have to check if it is a power of 2 or not. 
 
 Examples
@@ -15,19 +17,9 @@ Constraints:
 0 ≤ n < 109
 
 */
-Given a non-negative integer n. You have to check if it is a power of 2 or not. 
-
-Examples
-
-Input: n = 8
-Output: true
-Explanation: 8 is equal to 2 raised to 3 (23 = 8).
-Input: n = 98
-Output: false
-Explanation: 98 cannot be obtained by any power of 2.
-Input: n = 1
-Output: true
-Explanation: (20 = 1).
-Constraints:
-0 ≤ n < 109
-
+class Solution {
+    public static boolean isPowerofTwo(int n) {
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0;
+    }
+}
