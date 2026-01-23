9cd12beaac59b291ce881b36c45058565ca57066
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-23 23:10:13 +0530

    COMMIT

--- LEETCODE/MEDIUM/Subsets_II_90.java
+++ LEETCODE/MEDIUM/Subsets_II_90.java
@@ -19,3 +19,26 @@ Constraints:
 1 <= nums.length <= 10
 -10 <= nums[i] <= 10
 */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), res);
+        return res;
+    }
+
+    void backtrack(int idx, int[] nums, List<Integer> cur, List<List<Integer>> res) {
+        res.add(new ArrayList<>(cur));
+        for (int i = idx; i < nums.length; i++) {
+            if (i > idx && nums[i] == nums[i - 1]) continue;
+            cur.add(nums[i]);
+            backtrack(i + 1, nums, cur, res);
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
