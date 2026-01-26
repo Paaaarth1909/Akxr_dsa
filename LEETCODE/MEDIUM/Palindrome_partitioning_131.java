d68367843ec2423aeb9305aeaa3950c39263ddd7
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-26 13:39:39 +0530

    COMMIT

--- LEETCODE/MEDIUM/Palindrome_partitioning_131.java
+++ LEETCODE/MEDIUM/Palindrome_partitioning_131.java
@@ -17,3 +17,35 @@ Constraints:
 1 <= s.length <= 16
 s contains only lowercase English letters.
 */
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        dfs(0, s, new ArrayList<>(), res);
+        return res;
+    }
+
+    void dfs(int idx, String s, List<String> cur, List<List<String>> res) {
+        if (idx == s.length()) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int i = idx; i < s.length(); i++) {
+            if (isPal(s, idx, i)) {
+                cur.add(s.substring(idx, i + 1));
+                dfs(i + 1, s, cur, res);
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+
+    boolean isPal(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
