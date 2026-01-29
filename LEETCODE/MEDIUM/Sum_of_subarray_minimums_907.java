a71a5e656d715eceea495517258a2f08951814de
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-29 23:40:50 +0530

    COMMIT

--- LEETCODE/MEDIUM/Sum_of_subarray_minimums_907.java
+++ LEETCODE/MEDIUM/Sum_of_subarray_minimums_907.java
@@ -22,3 +22,38 @@ Constraints:
 1 <= arr[i] <= 3 * 104
 */
 
+import java.util.Stack;
+
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int mod = 1_000_000_007;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) st.pop();
+            left[i] = st.isEmpty() ? i + 1 : i - st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+            right[i] = st.isEmpty() ? n - i : st.peek() - i;
+            st.push(i);
+        }
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = (ans + (long) arr[i] * left[i] * right[i]) % mod;
+        }
+
+        return (int) ans;
+    }
+}
+
