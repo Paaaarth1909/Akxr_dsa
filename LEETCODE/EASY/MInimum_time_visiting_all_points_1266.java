03559f22e28f0b64ded965317e8eb4cd9e2174a9
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-04 23:47:11 +0530

    COMMIT

--- LEETCODE/EASY/MInimum_time_visiting_all_points_1266.java
+++ LEETCODE/EASY/MInimum_time_visiting_all_points_1266.java
@@ -32,3 +32,14 @@ points.length == n
 points[i].length == 2
 -1000 <= points[i][0], points[i][1] <= 1000
 */
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+        for (int i = 1; i < points.length; i++) {
+            int dx = Math.abs(points[i][0] - points[i - 1][0]);
+            int dy = Math.abs(points[i][1] - points[i - 1][1]);
+            time += Math.max(dx, dy);
+        }
+        return time;
+    }
+}
