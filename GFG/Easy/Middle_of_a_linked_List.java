bd83f7e545827ff1f62f2326032468b281316fdb
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-04 23:54:09 +0530

    COMMIT

--- GFG/Easy/Middle_of_a_linked_List.java
+++ GFG/Easy/Middle_of_a_linked_List.java
@@ -1,1 +1,20 @@
-/*  */
+/* You are given the head of a linked list, You have to return the value of the middle node of the linked list.
+
+If the number of nodes is odd, return the middle node value.
+If the number of nodes is even, there are two middle nodes, so return the second middle node value.
+Examples:
+
+Input: 
+   
+Output: 3
+Explanation: The given linked list is 1->2->3->4->5 and its middle is 3.
+   
+Input:
+   
+Output: 7 
+Explanation: The given linked list is 2->4->6->7->5->1 so, there are two middle node 6 and 7, return the second middle node as 7.
+   
+Constraints:
+1 ≤ no. of nodes ≤ 105
+1 ≤ node->data ≤ 105
+*/
