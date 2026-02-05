e43539de9565fbd26a3f43dafb6e0c701739c0ac
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-05 15:14:51 +0530

    COMMIT

--- LEETCODE/MEDIUM/Divide_two_integers_29.java
+++ LEETCODE/MEDIUM/Divide_two_integers_29.java
@@ -25,3 +25,25 @@ Constraints:
 -231 <= dividend, divisor <= 231 - 1
 divisor != 0
 */
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+
+        int res = 0;
+        while (a >= b) {
+            long temp = b, mul = 1;
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                mul <<= 1;
+            }
+            a -= temp;
+            res += mul;
+        }
+
+        return (dividend > 0) ^ (divisor > 0) ? -res : res;
+    }
+}
