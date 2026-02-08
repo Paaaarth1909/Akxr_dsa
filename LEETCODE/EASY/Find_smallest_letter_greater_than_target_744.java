253ec9086380c9d2a5bc451349ba256de0ac794f
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-08 21:49:58 +0530

    COMMIT

--- LEETCODE/EASY/Find_smallest_letter_greater_than_target_744.java
+++ LEETCODE/EASY/Find_smallest_letter_greater_than_target_744.java
@@ -1,3 +1,5 @@
+package LEETCODE.EASY;
+
 /* You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
 
 Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
@@ -30,3 +32,14 @@ letters contains at least two different characters.
 target is a lowercase English letter.
  
 */
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int l = 0, r = letters.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (letters[m] <= target) l = m + 1;
+            else r = m - 1;
+        }
+        return letters[l % letters.length];
+    }
+}
