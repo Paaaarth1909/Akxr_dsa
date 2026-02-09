13ed3b681d950fddeaa1034e3abf53975339678a
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-09 22:52:42 +0530

    COMMIT

--- LEETCODE/EASY/Minimum_bit_flips_to_convert_number_2220.java
+++ LEETCODE/EASY/Minimum_bit_flips_to_convert_number_2220.java
@@ -29,3 +29,14 @@ Constraints:
 
 0 <= start, goal <= 109
 */
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int x = start ^ goal;
+        int cnt = 0;
+        while (x != 0) {
+            cnt += x & 1;
+            x >>= 1;
+        }
+        return cnt;
+    }
+}
