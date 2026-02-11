de27cf77445f033363c2731644bd006b599a900f
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-11 23:46:01 +0530

    COMMIT

--- GFG/Easy/Floor_in_a_sorted_array.java
+++ GFG/Easy/Floor_in_a_sorted_array.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
 
 Note: In case of multiple occurrences of floor of x, return the index of the last occurrence.
@@ -19,3 +21,22 @@ Constraints:
 0 ≤ x ≤ arr[n-1]
 
 */
+class Solution {
+    public int findFloor(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] <= x) {
+                ans = mid;      // possible floor
+                low = mid + 1;  // move right for last occurrence
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
