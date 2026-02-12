7889022b96a8f3d22235519f9dcda036f3ad62a6
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-12 22:17:42 +0530

    COMMIT

--- LEETCODE/MEDIUM/Word_break_139.java
+++ LEETCODE/MEDIUM/Word_break_139.java
@@ -29,3 +29,25 @@ Constraints:
 s and wordDict[i] consist of only lowercase English letters.
 All the strings of wordDict are unique.
 */
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
