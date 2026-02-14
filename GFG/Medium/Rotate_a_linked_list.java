725031e9bbee67a649379eaf061813c0b2bb9534
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-14 18:26:34 +0530

    COMMIT

--- GFG/Medium/Rotate_a_linked_list.java
+++ GFG/Medium/Rotate_a_linked_list.java
@@ -1,1 +1,23 @@
-/*  */
+/* You are given the head of a singly linked list, you have to left rotate the linked list k times. Return the head of the modified linked list.
+
+Examples:
+
+Input: k = 4,
+   
+Output: 50 -> 10 -> 20 -> 30 -> 40
+Explanation:
+Rotate 1: 20 -> 30 -> 40 -> 50 -> 10
+Rotate 2: 30 -> 40 -> 50 -> 10 -> 20
+Rotate 3: 40 -> 50 -> 10 -> 20 -> 30
+Rotate 4: 50 -> 10 -> 20 -> 30 -> 40
+   
+Input: k = 6,
+   
+Output: 30 -> 40 -> 10 -> 20 
+   
+Constraints:
+1 ≤ number of nodes ≤ 105
+0 ≤ k ≤ 109
+0 ≤ node.data ≤ 109
+
+*/
