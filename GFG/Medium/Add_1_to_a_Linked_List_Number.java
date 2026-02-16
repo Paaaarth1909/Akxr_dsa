2c1a4f900997827d40a5345685d09dc05c00663e
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-16 23:09:45 +0530

    COMMIT

--- GFG/Medium/Add_1_to_a_Linked_List_Number.java
+++ GFG/Medium/Add_1_to_a_Linked_List_Number.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* You are given a linked list where each element in the list is a node and have an integer data. You need to add 1 to the number formed by concatinating all the list node numbers together and return the head of the modified linked list. 
 
 Note: The head represents the first element of the given array.
@@ -20,3 +22,49 @@ Constraints:
 0 <= list[i] <= 9
 
 */
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node addOne(Node head) {
+        head = reverse(head);
+
+        Node curr = head;
+        int carry = 1;
+
+        while (curr != null && carry > 0) {
+            int sum = curr.data + carry;
+            curr.data = sum % 10;
+            carry = sum / 10;
+
+            if (curr.next == null && carry > 0) {
+                curr.next = new Node(carry);
+                carry = 0;
+                break;
+            }
+            curr = curr.next;
+        }
+
+        return reverse(head);
+    }
+
+    private Node reverse(Node head) {
+        Node prev = null, curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
