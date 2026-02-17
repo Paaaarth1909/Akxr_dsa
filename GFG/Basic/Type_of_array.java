56d8b45de8de1d9c4e296ffa42cc8515a35b3283
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-17 23:45:18 +0530

    COMMIT

--- GFG/Basic/Type_of_array.java
+++ GFG/Basic/Type_of_array.java
@@ -1,1 +1,20 @@
-/*  */
+/* You are given an array arr[] having unique elements. Your task is to return the type of array.
+Note: The array can be categorized into ascending, descending, descending rotated and ascending rotated followed by:
+
+Return 1 if the array is in ascending order
+Return 2 if the array is in descending order
+Return 3 if the array is in descending rotated order
+Return 4 if the array is in ascending rotated order
+Examples:
+
+Input: arr[] = [2, 1, 5, 4, 3]
+Output: 3
+Explanation: Descending rotated, rotate 2 times left.
+Input: arr[] = [3, 4, 5, 1, 2]
+Output: 4
+Explanation: Ascending rotated, rotate 2 times right. 
+Constraints:
+3 <= arr.size() <= 105
+1 <= arr[i] <= 106
+
+*/
