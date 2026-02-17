690802797a98feb1ad185fc1109dd29f66d41aff
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-17 23:45:35 +0530

    COMMIT

--- GFG/Basic/Type_of_array.java
+++ GFG/Basic/Type_of_array.java
@@ -1,3 +1,5 @@
+package GFG.Basic;
+
 /* You are given an array arr[] having unique elements. Your task is to return the type of array.
 Note: The array can be categorized into ascending, descending, descending rotated and ascending rotated followed by:
 
@@ -18,3 +20,23 @@ Constraints:
 1 <= arr[i] <= 106
 
 */
+class Solution {
+    int maxNtype(int arr[]) {
+        int n = arr.length;
+        int inc = 0, dec = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) inc++;
+            else dec++;
+        }
+
+        if (dec == 0) return 1;   // ascending
+        if (inc == 0) return 2;   // descending
+
+        // Rotated cases
+        if (dec == 1 && arr[n - 1] < arr[0]) return 4;  // ascending rotated
+        if (inc == 1 && arr[n - 1] > arr[0]) return 3;  // descending rotated
+
+        return -1; // fallback (shouldn't happen per constraints)
+    }
+}
