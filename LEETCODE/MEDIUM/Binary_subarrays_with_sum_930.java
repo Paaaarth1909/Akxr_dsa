1724c22843c703e747920b27e1509e8fd44d78d2
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-19 23:20:09 +0530

    COMMIT

--- LEETCODE/MEDIUM/Binary_subarrays_with_sum_930.java
+++ LEETCODE/MEDIUM/Binary_subarrays_with_sum_930.java
@@ -26,3 +26,25 @@ nums[i] is either 0 or 1.
 0 <= goal <= nums.length
  
 */
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    private int atMost(int[] nums, int goal) {
+        if (goal < 0) return 0;
+        int l = 0, sum = 0, count = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            sum += nums[r];
+
+            while (sum > goal) {
+                sum -= nums[l++];
+            }
+
+            count += r - l + 1;
+        }
+
+        return count;
+    }
+}
