453e970738462bbd88aa217c990b2a7be695494e
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-21 23:41:30 +0530

    COMMIT

--- GFG/Medium/Unique_number_II.java
+++ GFG/Medium/Unique_number_II.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given an array arr[] containing 2*n + 2 positive numbers, out of which 2*n numbers exist in pairs whereas only two number occur exactly once and are distinct. Find the other two numbers. Return the answer in increasing order.
 
 Examples:
@@ -17,3 +19,33 @@ Constraints:
 arr.size() is even
 
 */
+class Solution {
+    public int[] singleNum(int[] arr) {
+        
+        int xor = 0;
+        
+        // Step 1: XOR all elements
+        for (int num : arr) {
+            xor ^= num;
+        }
+        
+        // Step 2: Get rightmost set bit
+        int diff = xor & (-xor);
+        
+        int num1 = 0, num2 = 0;
+        
+        // Step 3: Divide into two groups
+        for (int num : arr) {
+            if ((num & diff) != 0)
+                num1 ^= num;
+            else
+                num2 ^= num;
+        }
+        
+        // Return in increasing order
+        if (num1 < num2)
+            return new int[]{num1, num2};
+        else
+            return new int[]{num2, num1};
+    }
+}
