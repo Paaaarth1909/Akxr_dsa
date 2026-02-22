cac9c8eb13cdd2ba53df6ed2aef9656b1b579d92
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-22 10:01:47 +0530

    COMMIT

--- LEETCODE/EASY/Reverse_bits_190.java
+++ LEETCODE/EASY/Reverse_bits_190.java
@@ -31,3 +31,14 @@ Constraints:
 0 <= n <= 231 - 2
 n is even.
 */
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>>= 1;
+        }
+        return res;
+    }
+}
