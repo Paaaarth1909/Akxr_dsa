d7a3df63f485fbbe521b3e0ca0130cc0aaa66b55
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-26 23:51:48 +0530

    COMMIT

--- GFG/Medium/Next_permutation.java
+++ GFG/Medium/Next_permutation.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers into the lexicographically next greater permutation. If no such permutation exists, rearrange the numbers into the lowest possible order (i.e., sorted in ascending order). 
 
 Note:  A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
@@ -18,3 +20,25 @@ Constraints:
 0 ≤ arr[i] ≤ 105
 
 */
+class Solution {
+    void nextPermutation(int[] arr) {
+        int n = arr.length;
+        int i=n-2;
+        while ( i>=0 &&arr[i]>=arr[i+1])i--;
+        if (i>=0){
+            int j=n-1;
+            while(arr[j]<=arr[i])j--;
+            swap(arr,i,j);
+        }
+        reverse(arr,i+1,n-1);
+    }
+    void swap(int[] arr,int i , int j ){
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+        
+    }
+    void reverse(int[] arr,int i , int j ){
+     while ( i<j)swap(arr,i++,j--);   
+    }
+}
