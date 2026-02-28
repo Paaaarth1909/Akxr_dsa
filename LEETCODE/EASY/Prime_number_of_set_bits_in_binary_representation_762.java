4aaeba7abedff560f886b38dd01215f3b9726d1a
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-28 23:02:42 +0530

    COMMIT

--- LEETCODE/EASY/Prime_number_of_set_bits_in_binary_representation_762.java
+++ LEETCODE/EASY/Prime_number_of_set_bits_in_binary_representation_762.java
@@ -35,3 +35,23 @@ Constraints:
 1 <= left <= right <= 106
 0 <= right - left <= 104
 */
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+
+        for (int i = left; i <= right; i++) {
+            int bits = Integer.bitCount(i);
+            if (isPrime(bits)) count++;
+        }
+
+        return count;
+    }
+
+    private boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
