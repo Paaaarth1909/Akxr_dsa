fcf1c8bf7111f264367ebfb0e884def786af4094
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-02 19:29:52 +0530

    COMMIT

---
+++ GFG/Basic/Fibonacci_in_the_array.java
@@ -0,0 +1,18 @@
+/* Given an array arr of integers, the task is to count the number of elements of the array which are Fibonacci numbers
+
+Examples:
+
+Input: arr[] = [4, 2, 8, 5, 20, 1, 40, 13, 23]
+Output: 5
+Explanation: Fibonacci Numbers that are present in array are 2, 8, 5, 1, 13
+Input: arr[] = [4, 7, 6, 25]
+Output: 0
+Explanation: No Fibonacci number in this array.
+Expected Time Complexity: O(n*log(maxElement))
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
+
+*/
