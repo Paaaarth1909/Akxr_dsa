d2bf43973d94d5ee6fb0329828f535fe101162e8
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-07 23:43:26 +0530

    COMMIT

--- LEETCODE/MEDIUM/Repeated_string_match_686.java
+++ LEETCODE/MEDIUM/Repeated_string_match_686.java
@@ -20,3 +20,23 @@ Constraints:
 1 <= a.length, b.length <= 104
 a and b consist of lowercase English letters.
 */
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        while (sb.length() < b.length()) {
+            sb.append(a);
+            count++;
+        }
+
+        if (sb.indexOf(b) != -1) return count;
+
+        sb.append(a);
+        count++;
+
+        if (sb.indexOf(b) != -1) return count;
+
+        return -1;
+    }
+}
