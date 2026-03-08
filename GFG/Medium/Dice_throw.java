a7fa7d7633806783645020fb213f1f1710c053ea
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-08 23:56:53 +0530

    COMMIT

--- GFG/Medium/Dice_throw.java
+++ GFG/Medium/Dice_throw.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given n dice each with m faces. Find the number of ways to get sum x which is the summation of values on each face when all the dice are thrown.
 
 Example:
@@ -12,3 +14,22 @@ Constraints:
 1 <= m,n,x <= 50
 
 */
+class Solution {
+    static int noOfWays(int m, int n, int x) {
+        
+        int[][] dp = new int[n + 1][x + 1];
+        
+        dp[0][0] = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= x; j++) {
+                
+                for (int k = 1; k <= m && k <= j; k++) {
+                    dp[i][j] += dp[i - 1][j - k];
+                }
+            }
+        }
+        
+        return dp[n][x];
+    }
+}
