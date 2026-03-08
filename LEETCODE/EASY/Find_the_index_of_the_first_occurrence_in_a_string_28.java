7a0d5dcb7707d94c400cf87baf8263c434c83942
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-08 23:54:12 +0530

    COMMIT

--- LEETCODE/EASY/Find_the_index_of_the_first_occurrence_in_a_string_28.java
+++ LEETCODE/EASY/Find_the_index_of_the_first_occurrence_in_a_string_28.java
@@ -21,3 +21,20 @@ Constraints:
 haystack and needle consist of only lowercase English characters.
  
 */
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+
+        int n = haystack.length(), m = needle.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == m) return i;
+        }
+
+        return -1;
+    }
+}
