822a6bbf42f5ad3220d317a81bb2901b61726597
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-11 23:53:55 +0530

    COMMIT

--- LEETCODE/EASY/MInimum_changes_to_make_alternating_binary_string-1758.java
+++ LEETCODE/EASY/MInimum_changes_to_make_alternating_binary_string-1758.java
@@ -28,3 +28,18 @@ Constraints:
 1 <= s.length <= 104
 s[i] is either '0' or '1'.
 */
+class Solution {
+    public int minOperations(String s) {
+        int c1 = 0, c2 = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char e1 = (i % 2 == 0) ? '0' : '1';
+            char e2 = (i % 2 == 0) ? '1' : '0';
+
+            if (s.charAt(i) != e1) c1++;
+            if (s.charAt(i) != e2) c2++;
+        }
+
+        return Math.min(c1, c2);
+    }
+}
