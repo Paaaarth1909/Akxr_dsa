971a2f0497bc023d01426cc09d1eebd407e216b7
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-13 21:55:29 +0530

    COMMIT

--- GFG/Medium/Generate_IP_Addresses.java
+++ GFG/Medium/Generate_IP_Addresses.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given a string s containing only digits, your task is to restore it by returning all possible valid IP address combinations. You can return your answer in any order.
 
 A valid IP address must be in the form of A.B.C.D, where A, B, C, and D are numbers from 0-255(inclusive).
@@ -18,3 +20,43 @@ Constraints:
 s contains only digits(i.e. 0-9)
 
 */
+import java.util.*;
+
+class Solution {
+    
+    public ArrayList<String> generateIp(String s) {
+        
+        ArrayList<String> res = new ArrayList<>();
+        backtrack(s, 0, 0, "", res);
+        return res;
+    }
+    
+    void backtrack(String s, int index, int parts, String curr, ArrayList<String> res){
+        
+        if(parts == 4 && index == s.length()){
+            res.add(curr.substring(0, curr.length() - 1)); // remove last dot
+            return;
+        }
+        
+        if(parts == 4 || index == s.length())
+            return;
+        
+        for(int len = 1; len <= 3 && index + len <= s.length(); len++){
+            
+            String segment = s.substring(index, index + len);
+            
+            if(isValid(segment)){
+                backtrack(s, index + len, parts + 1, curr + segment + ".", res);
+            }
+        }
+    }
+    
+    boolean isValid(String segment){
+        
+        if(segment.length() > 1 && segment.charAt(0) == '0')
+            return false;
+        
+        int val = Integer.parseInt(segment);
+        return val >= 0 && val <= 255;
+    }
+}
