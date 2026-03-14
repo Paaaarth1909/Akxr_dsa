9c657b73dd9b5eadffcc4c6d760d4c18541e8aea
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-14 23:39:39 +0530

    COMMIT

---
+++ GFG/Medium/Top_view_of_binary_tree.java
@@ -0,0 +1,87 @@
+package GFG.Medium;
+
+/* You are given the root of a binary tree, and your task is to return its top view. The top view of a binary tree is the set of nodes visible when the tree is viewed from the top.
+
+Note:
+
+Return the nodes from the leftmost node to the rightmost node.
+If multiple nodes overlap at the same horizontal position, only the topmost (closest to the root) node is included in the view. 
+Examples:
+
+Input: root = [1, 2, 3]
+Output: [2, 1, 3]
+Explanation: The Green colored nodes represents the top view in the below Binary tree.
+ 
+Input: root = [10, 20, 30, 40, 60, 90, 100]
+Output: [40, 20, 10, 30, 100]
+Explanation: The Green colored nodes represents the top view in the below Binary tree.
+
+
+Constraints:
+1 ≤ number of nodes ≤ 105
+1 ≤ node->data ≤ 105
+
+*/
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution {
+    
+    class Pair {
+        Node node;
+        int hd;
+        
+        Pair(Node node, int hd){
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+    
+    public ArrayList<Integer> topView(Node root) {
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        if(root == null)
+            return result;
+        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        
+        q.add(new Pair(root, 0));
+        
+        while(!q.isEmpty()){
+            
+            Pair curr = q.poll();
+            Node node = curr.node;
+            int hd = curr.hd;
+            
+            // store only first node at this horizontal distance
+            if(!map.containsKey(hd)){
+                map.put(hd, node.data);
+            }
+            
+            if(node.left != null)
+                q.add(new Pair(node.left, hd - 1));
+            
+            if(node.right != null)
+                q.add(new Pair(node.right, hd + 1));
+        }
+        
+        for(int val : map.values()){
+            result.add(val);
+        }
+        
+        return result;
+    }
+}
