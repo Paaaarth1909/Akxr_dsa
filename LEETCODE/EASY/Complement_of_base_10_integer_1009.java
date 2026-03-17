b1fbcfc1d143e25b5763d80948d5c71dff9f5458
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-17 22:56:29 +0530

    COMMIT

--- LEETCODE/EASY/Complement_of_base_10_integer_1009.java
+++ LEETCODE/EASY/Complement_of_base_10_integer_1009.java
@@ -26,3 +26,18 @@ Constraints:
 
 0 <= n < 109
 */
+class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+
+        int mask = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+
+        return mask ^ n;
+    }
+}
