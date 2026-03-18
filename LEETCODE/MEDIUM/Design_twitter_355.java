f4af7a011f4c3fc30f228a197078fb072bceaef9
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-18 23:30:46 +0530

    COMMIT

---
+++ LEETCODE/MEDIUM/Design_twitter_355.java
@@ -0,0 +1,108 @@
+/* Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
+
+Implement the Twitter class:
+
+Twitter() Initializes your twitter object.
+void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. Each call to this function will be made with a unique tweetId.
+List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user themself. Tweets must be ordered from most recent to least recent.
+void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+ 
+
+Example 1:
+
+Input
+["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+[[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+Output
+[null, null, [5], null, null, [6, 5], null, [5]]
+
+Explanation
+Twitter twitter = new Twitter();
+twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+twitter.follow(1, 2);    // User 1 follows user 2.
+twitter.postTweet(2, 6); // User 2 posts a new tweet (id = 6).
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+twitter.unfollow(1, 2);  // User 1 unfollows user 2.
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.
+ 
+
+Constraints:
+
+1 <= userId, followerId, followeeId <= 500
+0 <= tweetId <= 104
+All the tweets have unique IDs.
+At most 3 * 104 calls will be made to postTweet, getNewsFeed, follow, and unfollow.
+A user cannot follow himself.
+*/
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+class Twitter {
+
+    private static int time = 0;
+
+    class Tweet {
+        int id, time;
+        Tweet next;
+
+        Tweet(int id) {
+            this.id = id;
+            this.time = Twitter.time++;
+        }
+    }
+
+    Map<Integer, Set<Integer>> followMap;
+    Map<Integer, Tweet> tweetMap;
+
+    public Twitter() {
+        followMap = new HashMap<>();
+        tweetMap = new HashMap<>();
+    }
+
+    public void postTweet(int userId, int tweetId) {
+        Tweet t = new Tweet(tweetId);
+        t.next = tweetMap.get(userId);
+        tweetMap.put(userId, t);
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> res = new ArrayList<>();
+
+        followMap.putIfAbsent(userId, new HashSet<>());
+        followMap.get(userId).add(userId);
+
+        PriorityQueue<Tweet> pq = new PriorityQueue<>((a, b) -> b.time - a.time);
+
+        for (int user : followMap.get(userId)) {
+            Tweet t = tweetMap.get(user);
+            if (t != null) pq.add(t);
+        }
+
+        while (!pq.isEmpty() && res.size() < 10) {
+            Tweet t = pq.poll();
+            res.add(t.id);
+            if (t.next != null) pq.add(t.next);
+        }
+
+        return res;
+    }
+
+    public void follow(int followerId, int followeeId) {
+        followMap.putIfAbsent(followerId, new HashSet<>());
+        followMap.get(followerId).add(followeeId);
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+        if (followMap.containsKey(followerId)) {
+            followMap.get(followerId).remove(followeeId);
+        }
+    }
+}
