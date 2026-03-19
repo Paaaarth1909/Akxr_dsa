beaefd1b7843920a653e68f2ed2f8ed02f251692
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-19 17:04:29 +0530

    COMMIT

--- LEETCODE/EASY/Kth_largest_element_in_a_stream_703.java
+++ LEETCODE/EASY/Kth_largest_element_in_a_stream_703.java
@@ -51,3 +51,28 @@ Constraints:
 At most 104 calls will be made to add.
  
 */
+
+import java.util.PriorityQueue;
+
+class KthLargest {
+
+    private PriorityQueue<Integer> pq;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+
+        for (int n : nums) {
+            add(n);
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > k) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
