d1ff2a1477d70ea63bb5885bae9afbe5d23e2c80
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-19 17:19:31 +0530

    COMMIT

---
+++ GFG/Medium/Construct_tree_from_inorder_&_preorder.java
@@ -0,0 +1,20 @@
+/* Given two arrays representing the inorder and preorder traversals of a binary tree, your task is to construct the binary tree and return its root.
+
+Note: The inorder and preorder traversals contain unique values, and every value present in the preorder traversal is also found in the inorder traversal.
+
+Examples:
+
+Input: inorder[] = [3, 1, 4, 0, 5, 2], preorder[] = [0, 1, 3, 4, 2, 5]
+Output: [0, 1, 2, 3, 4, 5]
+Explanation: The tree will look like
+
+Input: inorder[] = [2, 5, 4, 1, 3], preorder[] = [1, 4, 5, 2, 3]
+Output: [1, 4, 3, 5, N, N, N, 2]
+Explanation: The tree will look like
+
+Constraints:
+1 ≤ number of nodes ≤ 103
+0 ≤ nodes -> data ≤ 103
+Both the inorder and preorder arrays contain unique values.
+
+*/
