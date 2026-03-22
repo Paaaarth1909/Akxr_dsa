b56d34b954cb2c1c5f36faa07fa58ab632d92a90
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-22 23:36:27 +0530

    COMMIT

--- LEETCODE/EASY/Binary_tree_postorder_traversal_145.java
+++ LEETCODE/EASY/Binary_tree_postorder_traversal_145.java
@@ -41,3 +41,39 @@ Constraints:
 The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 */
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+  public class Binary_tree_postorder_traversal_145 {
+      int val;
+      Binary_tree_postorder_traversal_145 left;
+      Binary_tree_postorder_traversal_145 right;
+      Binary_tree_postorder_traversal_145() {}
+      Binary_tree_postorder_traversal_145(int val) { this.val = val; }
+      Binary_tree_postorder_traversal_145(int val, Binary_tree_postorder_traversal_145 left, Binary_tree_postorder_traversal_145 right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    public List<Integer> postorderTraversal(Binary_tree_postorder_traversal_145 root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    private void postorder(Binary_tree_postorder_traversal_145 node, List<Integer> res) {
+        if (node == null) return;
+
+        postorder(node.left, res);
+        postorder(node.right, res);
+        res.add(node.val);
+    }
+}
