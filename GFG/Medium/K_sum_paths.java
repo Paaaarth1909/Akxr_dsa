0b7cf0fbdfd1690ad547ea750821bd15fd0db51a
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-23 10:10:19 +0530

    COMMIT

--- GFG/Medium/K_sum_paths.java
+++ GFG/Medium/K_sum_paths.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given the root of a binary tree and an integer k, determine the number of downward-only paths where the sum of the node values in the path equals k.
 
 Note: A path can start and end at any node within the tree but must always move downward (from parent to child).
@@ -20,3 +22,53 @@ Constraints:
 -109 ≤ k ≤ 109
 
 */
+import java.util.*;
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+    
+    int count = 0;
+    
+    public int countAllPaths(Node root, int k) {
+        
+        HashMap<Long, Integer> map = new HashMap<>();
+        map.put(0L, 1); // base case
+        
+        dfs(root, 0, k, map);
+        
+        return count;
+    }
+    
+    void dfs(Node node, long currSum, int k, HashMap<Long, Integer> map) {
+        
+        if(node == null)
+            return;
+        
+        currSum += node.data;
+        
+        // check if there exists a path ending here
+        if(map.containsKey(currSum - k)) {
+            count += map.get(currSum - k);
+        }
+        
+        // add current sum
+        map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+        
+        // recurse
+        dfs(node.left, currSum, k, map);
+        dfs(node.right, currSum, k, map);
+        
+        // backtrack
+        map.put(currSum, map.get(currSum) - 1);
+    }
+}
