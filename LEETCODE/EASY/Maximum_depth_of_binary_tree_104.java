5ce7664ddaa43808cba27470b90bebb92017a8b5
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-24 23:01:02 +0530

    COMMIT

--- LEETCODE/EASY/Maximum_depth_of_binary_tree_104.java
+++ LEETCODE/EASY/Maximum_depth_of_binary_tree_104.java
@@ -19,3 +19,24 @@ The number of nodes in the tree is in the range [0, 104].
 -100 <= Node.val <= 100
  
 */
+
+ 
+ public class Maximum_depth_of_binary_tree_104 {
+      int val;
+      Maximum_depth_of_binary_tree_104 left;
+      Maximum_depth_of_binary_tree_104 right;
+      Maximum_depth_of_binary_tree_104() {}
+      Maximum_depth_of_binary_tree_104(int val) { this.val = val; }
+      Maximum_depth_of_binary_tree_104(int val, Maximum_depth_of_binary_tree_104 left, Maximum_depth_of_binary_tree_104 right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+  
+class Solution {
+    public int maxDepth(Maximum_depth_of_binary_tree_104 root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
