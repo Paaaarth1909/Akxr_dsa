51244e5f6e4c4d4a7f5484ebc2d781956def35a8
Parthsaarthie sharma <toshit113@gmail.com>
2026-03-30 19:53:07 +0530

    COMMIT

--- LEETCODE/EASY/Same_tree_100.java
+++ LEETCODE/EASY/Same_tree_100.java
@@ -27,3 +27,25 @@ The number of nodes in both trees is in the range [0, 100].
 -104 <= Node.val <= 104
  
 */
+ public class Same_tree_100 {
+         int val;
+         Same_tree_100 left;
+         Same_tree_100 right;
+         Same_tree_100() {}
+         Same_tree_100(int val) { this.val = val; }
+         Same_tree_100(int val, Same_tree_100 left, Same_tree_100 right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+ 
+class Solution {
+    public boolean isSameTree(Same_tree_100 p, Same_tree_100 q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
