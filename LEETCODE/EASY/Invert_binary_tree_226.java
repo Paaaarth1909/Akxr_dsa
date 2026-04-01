3a6e12b7608ad790ea6a9d4a5eba019d608ba0b5
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-01 20:40:43 +0530

    COMMIT

--- LEETCODE/EASY/Invert_binary_tree_226.java
+++ LEETCODE/EASY/Invert_binary_tree_226.java
@@ -23,3 +23,30 @@ Constraints:
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 */
+
+  public class Invert_binary_tree_226 {
+      int val;
+      Invert_binary_tree_226 left;
+      Invert_binary_tree_226 right;
+      Invert_binary_tree_226() {}
+      Invert_binary_tree_226(int val) { this.val = val; }
+      Invert_binary_tree_226(int val, Invert_binary_tree_226 left, Invert_binary_tree_226 right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    public Invert_binary_tree_226 invertTree(Invert_binary_tree_226 root) {
+        if (root == null) return null;
+
+        Invert_binary_tree_226 left = invertTree(root.left);
+        Invert_binary_tree_226 right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
