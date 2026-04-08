ceda6df0986aea4008b5c7d99995a27bfb900c3d
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-08 19:05:35 +0530

    COMMIT

--- LEETCODE/MEDIUM/Kth_smallest_element_in_a_BST_230.java
+++ LEETCODE/MEDIUM/Kth_smallest_element_in_a_BST_230.java
@@ -20,3 +20,37 @@ The number of nodes in the tree is n.
 1 <= k <= n <= 104
 0 <= Node.val <= 104
 */
+
+import java.util.Stack;
+
+
+  public class Kth_smallest_element_in_a_BST_230 {
+      int val;
+      Kth_smallest_element_in_a_BST_230 left;
+      Kth_smallest_element_in_a_BST_230 right;
+      Kth_smallest_element_in_a_BST_230() {}
+      Kth_smallest_element_in_a_BST_230(int val) { this.val = val; }
+      Kth_smallest_element_in_a_BST_230(int val, Kth_smallest_element_in_a_BST_230 left, Kth_smallest_element_in_a_BST_230 right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    public int kthSmallest(Kth_smallest_element_in_a_BST_230 root, int k) {
+        Stack<Kth_smallest_element_in_a_BST_230> stack = new Stack<>();
+
+        while (true) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            if (--k == 0) return root.val;
+
+            root = root.right;
+        }
+    }
+}
