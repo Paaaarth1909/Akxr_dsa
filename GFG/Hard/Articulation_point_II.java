db89d3424de327d8b3287b8de75540626559986c
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-13 22:13:12 +0530

    COMMIT

--- GFG/Hard/Articulation_point_II.java
+++ GFG/Hard/Articulation_point_II.java
@@ -1,3 +1,5 @@
+package GFG.Hard;
+
 /* You are given an undirected graph with V vertices and E edges. The graph is represented as a 2D array edges[][], where each element edges[i] = [u, v] indicates an undirected edge between vertices u and v.
 Your task is to return all the articulation points (or cut vertices) in the graph.
 An articulation point is a vertex whose removal, along with all its connected edges, increases the number of connected components in the graph.
@@ -19,3 +21,87 @@ Constraints:
 1 ≤ V, E ≤ 104
 
 */
+import java.util.*;
+
+class Solution {
+    
+    static int timer;
+    
+    static ArrayList<Integer> articulationPoints(int V, int[][] edges) {
+        
+        // Step 1: build graph
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] e : edges){
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        
+        int[] tin = new int[V];
+        int[] low = new int[V];
+        boolean[] visited = new boolean[V];
+        boolean[] isAP = new boolean[V];
+        
+        timer = 0;
+        
+        // handle multiple components
+        for(int i = 0; i < V; i++){
+            if(!visited[i]){
+                dfs(i, -1, adj, tin, low, visited, isAP);
+            }
+        }
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        for(int i = 0; i < V; i++){
+            if(isAP[i]) res.add(i);
+        }
+        
+        if(res.size() == 0){
+            res.add(-1);
+        }
+        
+        return res;
+    }
+    
+    static void dfs(int node, int parent,
+                    ArrayList<ArrayList<Integer>> adj,
+                    int[] tin, int[] low,
+                    boolean[] visited, boolean[] isAP){
+        
+        visited[node] = true;
+        tin[node] = low[node] = timer++;
+        
+        int children = 0;
+        
+        for(int neighbor : adj.get(node)){
+            
+            if(neighbor == parent) continue;
+            
+            if(!visited[neighbor]){
+                
+                dfs(neighbor, node, adj, tin, low, visited, isAP);
+                
+                low[node] = Math.min(low[node], low[neighbor]);
+                
+                // Case 2: articulation condition
+                if(parent != -1 && low[neighbor] >= tin[node]){
+                    isAP[node] = true;
+                }
+                
+                children++;
+            }
+            else{
+                low[node] = Math.min(low[node], tin[neighbor]);
+            }
+        }
+        
+        // Case 1: root node
+        if(parent == -1 && children > 1){
+            isAP[node] = true;
+        }
+    }
+}
