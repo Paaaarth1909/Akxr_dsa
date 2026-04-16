485a7c50951ffc56c748de8a18851db174488a62
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-16 22:59:42 +0530

    COMMIT

--- LEETCODE/MEDIUM/Rotting_oranges_994.java
+++ LEETCODE/MEDIUM/Rotting_oranges_994.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* You are given an m x n grid where each cell can have one of three values:
 
 0 representing an empty cell,
@@ -34,3 +36,56 @@ n == grid[i].length
 grid[i][j] is 0, 1, or 2.
  
 */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0;
+
+        // Step 1: Count fresh oranges & add rotten to queue
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        if (fresh == 0) return 0;
+
+        int minutes = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        // Step 2: BFS (level order)
+        while (!q.isEmpty()) {
+            int size = q.size();
+            boolean rotted = false;
+
+            for (int i = 0; i < size; i++) {
+                int[] cur = q.poll();
+
+                for (int[] d : dirs) {
+                    int x = cur[0] + d[0];
+                    int y = cur[1] + d[1];
+
+                    if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        q.offer(new int[]{x, y});
+                        fresh--;
+                        rotted = true;
+                    }
+                }
+            }
+
+            if (rotted) minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+}
