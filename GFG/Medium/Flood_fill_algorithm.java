74332d6c291dcf09992731e59843735a66c7bdc9
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-16 22:55:37 +0530

    COMMIT

--- GFG/Medium/Flood_fill_algorithm.java
+++ GFG/Medium/Flood_fill_algorithm.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* You are given a 2D grid image[][], where each image[i][j] represents the color of a pixel in the image. Also provided a coordinate (sr, sc) representing the starting pixel (row and column) and a new color value newColor.
 
 Your task is to perform a flood fill starting from the pixel (sr, sc), changing its color to newColor and the color of all the connected pixels that have the same original color. Two pixels are considered connected if they are adjacent horizontally or vertically (not diagonally) and have the same original color.
@@ -19,3 +21,39 @@ Constraints:
 0 ≤ sr ≤ (n-1)
 0 ≤ sc ≤ (m-1)
 */
+class Solution {
+    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        
+        int original = image[sr][sc];
+        
+        // edge case
+        if(original == newColor) return image;
+        
+        dfs(image, sr, sc, original, newColor);
+        
+        return image;
+    }
+    
+    void dfs(int[][] image, int i, int j, int original, int newColor){
+        
+        int n = image.length;
+        int m = image[0].length;
+        
+        // boundary + condition check
+        if(i < 0 || j < 0 || i >= n || j >= m || image[i][j] != original){
+            return;
+        }
+        
+        // color change
+        image[i][j] = newColor;
+        
+        // explore 4 directions
+        for(int d = 0; d < 4; d++){
+            dfs(image, i + dx[d], j + dy[d], original, newColor);
+        }
+    }
+}
