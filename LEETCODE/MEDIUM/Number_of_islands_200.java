7e31cb2600b161f2f5743f87f7a452ee8150a9fe
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-19 13:19:46 +0530

    COMMIT

--- LEETCODE/MEDIUM/Number_of_islands_200.java
+++ LEETCODE/MEDIUM/Number_of_islands_200.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
@@ -32,3 +34,33 @@ n == grid[i].length
 grid[i][j] is '0' or '1'.
  
 */
+class Solution {
+    public int numIslands(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') return;
+
+        grid[i][j] = '0'; // mark visited
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
