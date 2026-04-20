941fb756f9dcc6f4ac390353bd52b8591503f5f8
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-20 21:58:04 +0530

    COMMIT

--- GFG/Medium/Count_derangements.java
+++ GFG/Medium/Count_derangements.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given a number n, find the total number of Derangements of elements from 1 to n. A Derangement is a permutation of n elements, such that no element appears in its original position, i.e., 1 should not be the first element, 2 should not be second, etc. For example, [5, 3, 2, 1, 4] is a Derangement of first 5 elements.
 
 Note: The answer will always fit into a 32-bit integer.
@@ -14,3 +16,22 @@ Constraints:
 1 ≤ n ≤ 12
 
 */
+class Solution {
+    public int derangeCount(int n) {
+        
+        if(n == 1) return 0;
+        if(n == 2) return 1;
+        
+        int prev2 = 0; // D(1)
+        int prev1 = 1; // D(2)
+        
+        for(int i = 3; i <= n; i++){
+            int curr = (i - 1) * (prev1 + prev2);
+            
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return prev1;
+    }
+}
