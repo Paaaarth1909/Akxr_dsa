e6e63fb425648f237494d4f6ff3c0475bbe72d62
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-24 22:51:47 +0530

    COMMIT

--- LEETCODE/MEDIUM/Surrounded_regions_130.java
+++ LEETCODE/MEDIUM/Surrounded_regions_130.java
@@ -33,3 +33,45 @@ n == board[i].length
 1 <= m, n <= 200
 board[i][j] is 'X' or 'O'.
 */
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length, n = board[0].length;
+
+        // Step 1: Mark border-connected 'O' as safe ('#')
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        // Step 2: Convert remaining 'O' → 'X', and '#' → 'O'
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';   // surrounded
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';   // safe
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        int m = board.length, n = board[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = '#'; // mark safe
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
