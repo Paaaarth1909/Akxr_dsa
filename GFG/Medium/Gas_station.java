9097be572f8da1a1dc95e0d9dbead6ca4067ddc7
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-26 18:51:42 +0530

    COMMIT

--- GFG/Medium/Gas_station.java
+++ GFG/Medium/Gas_station.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* There are n gas stations along a circular tour. You are given two integer arrays gas[] and cost[], where gas[i] is the amount of gas available at station i and cost[i] is the gas needed to travel from station i to station (i+1). You have a car with an unlimited gas tank and start with an empty tank at some station. Your task is to return the index of the starting station if it is possible to travel once around the circular route in a clockwise direction without running out of gas at any station; otherwise, return -1.
 
 Note: If a solution exists, it is guaranteed to be unique.
@@ -19,3 +21,27 @@ Constraints:
 1 ≤ gas[i], cost[i] ≤ 103
 
 */
+class Solution {
+    public int startStation(int[] gas, int[] cost) {
+        
+        int total = 0;
+        int curr = 0;
+        int start = 0;
+        
+        for(int i = 0; i < gas.length; i++){
+            
+            int diff = gas[i] - cost[i];
+            
+            total += diff;
+            curr += diff;
+            
+            // if we fail here
+            if(curr < 0){
+                start = i + 1;
+                curr = 0;
+            }
+        }
+        
+        return total >= 0 ? start : -1;
+    }
+}
