7270eb1bb7658ff6fb7076fce7ccd26631db38a0
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-27 22:35:39 +0530

    COMMIT

--- GFG/Easy/Smallest_window_containing_0,1_and_2.java
+++ GFG/Easy/Smallest_window_containing_0,1_and_2.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given a string s consisting only of the characters '0', '1' and '2', determine the length of the smallest substring that contains all three characters at least once.
 
 If no such substring exists, return -1.
@@ -14,3 +16,28 @@ Constraints:
 1 ≤ s.size() ≤ 105
 
 */
+class Solution {
+    public int smallestSubstring(String s) {
+        
+        int[] count = new int[3]; // for '0','1','2'
+        int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        
+        for(int right = 0; right < s.length(); right++){
+            
+            count[s.charAt(right) - '0']++;
+            
+            // check if window is valid
+            while(count[0] > 0 && count[1] > 0 && count[2] > 0){
+                
+                minLen = Math.min(minLen, right - left + 1);
+                
+                // shrink window
+                count[s.charAt(left) - '0']--;
+                left++;
+            }
+        }
+        
+        return minLen == Integer.MAX_VALUE ? -1 : minLen;
+    }
+}
