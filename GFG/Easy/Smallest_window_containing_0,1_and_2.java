d72135c6668d576608ceecd8c46d37d5d70bcb3c
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-27 22:35:16 +0530

    COMMIT

---
+++ GFG/Easy/Smallest_window_containing_0,1_and_2.java
@@ -0,0 +1,16 @@
+/* Given a string s consisting only of the characters '0', '1' and '2', determine the length of the smallest substring that contains all three characters at least once.
+
+If no such substring exists, return -1.
+
+Examples :
+
+Input: s = "10212"
+Output: 3
+Explanation: The substring "102" is the shortest substring that contains all three characters '0', '1', and '2', so the answer is 3.
+Input: s = "12121"
+Output: -1
+Explanation: The character '0' is not present in the string, so no substring can contain all three characters '0', '1', and '2'. Hence, the answer is -1.
+Constraints:
+1 ≤ s.size() ≤ 105
+
+*/
