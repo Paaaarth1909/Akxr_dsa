b786f2a5af567162db935381aaada0440027f5ed
Parthsaarthie sharma <toshit113@gmail.com>
2026-04-29 20:07:44 +0530

    COMMIT

--- GFG/Medium/Min_swaps_to_Group_1s.java
+++ GFG/Medium/Min_swaps_to_Group_1s.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* You are given a binary array arr[] consisting only of 0s and 1s. Determine the minimum number of swaps required to group all the 1s together in a contiguous subarray.
 
 A swap operation allows you to choose any two indices i and j and exchange their values (i.e., swap(arr[i], arr[j])).
@@ -20,3 +22,40 @@ Constraints:
 0 ≤ arr[i] ≤ 1
 
 */
+class Solution {
+    public int minSwaps(int[] arr) {
+        
+        int n = arr.length;
+        
+        // Step 1: count total 1s
+        int k = 0;
+        for(int num : arr){
+            if(num == 1) k++;
+        }
+        
+        // if no 1s
+        if(k == 0) return -1;
+        
+        // Step 2: sliding window
+        int currOnes = 0;
+        
+        // first window
+        for(int i = 0; i < k; i++){
+            if(arr[i] == 1) currOnes++;
+        }
+        
+        int maxOnes = currOnes;
+        
+        // slide window
+        for(int i = k; i < n; i++){
+            
+            if(arr[i] == 1) currOnes++;
+            if(arr[i - k] == 1) currOnes--;
+            
+            maxOnes = Math.max(maxOnes, currOnes);
+        }
+        
+        // Step 3: result
+        return k - maxOnes;
+    }
+}
