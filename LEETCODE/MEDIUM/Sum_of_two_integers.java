dcb3286bc7ec372ccf2fbdd08f8093fa13f24087
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-05 18:56:57 +0530

    COMMIT

--- LEETCODE/MEDIUM/Sum_of_two_integers.java
+++ LEETCODE/MEDIUM/Sum_of_two_integers.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* Given two integers a and b, return the sum of the two integers without using the operators + and -.
 
  
@@ -17,3 +19,13 @@ Constraints:
 -1000 <= a, b <= 1000
  
 */
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1; 
+            a = a ^ b;                
+            b = carry;               
+        }
+        return a;
+    }
+}
