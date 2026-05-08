f61237da031740ab3ad45d00bee90bcb36e373dd
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-08 23:29:17 +0530

    COMMIT

--- LEETCODE/MEDIUM/Gas_station_134.java
+++ LEETCODE/MEDIUM/Gas_station_134.java
@@ -39,3 +39,23 @@ n == gas.length == cost.length
 The input is generated such that the answer is unique.
  
 */
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;   
+        int tank = 0;    
+        int start = 0;   
+
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank += diff;
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return total >= 0 ? start : -1;
+    }
+}
