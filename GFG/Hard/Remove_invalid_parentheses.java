88b2e2f5c934f5fadba4c65d7d8f9969a46113c8
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-08 23:34:01 +0530

    COMMIT

--- GFG/Hard/Remove_invalid_parentheses.java
+++ GFG/Hard/Remove_invalid_parentheses.java
@@ -1,3 +1,5 @@
+package GFG.Hard;
+
 /* Given a string s consisting of lowercase letters and parentheses '(' and ')'.
 
 A string is considered valid if:
@@ -29,3 +31,72 @@ Constraints:
 s consists of lowercase English letters and parentheses '(' and ')'
 
 */
+import java.util.*;
+
+class Solution {
+    
+    public List<String> validParenthesis(String s) {
+        
+        List<String> res = new ArrayList<>();
+        
+        Queue<String> q = new LinkedList<>();
+        HashSet<String> visited = new HashSet<>();
+        
+        q.offer(s);
+        visited.add(s);
+        
+        boolean found = false;
+        
+        while(!q.isEmpty()){
+            
+            String curr = q.poll();
+            
+            // check valid
+            if(isValid(curr)){
+                res.add(curr);
+                found = true;
+            }
+            
+            // stop deeper levels once found
+            if(found) continue;
+            
+            for(int i = 0; i < curr.length(); i++){
+                
+                char ch = curr.charAt(i);
+                
+                // remove only parentheses
+                if(ch != '(' && ch != ')') continue;
+                
+                String next = curr.substring(0, i) +
+                              curr.substring(i + 1);
+                
+                if(!visited.contains(next)){
+                    visited.add(next);
+                    q.offer(next);
+                }
+            }
+        }
+        
+        Collections.sort(res);
+        return res;
+    }
+    
+    private boolean isValid(String s){
+        
+        int count = 0;
+        
+        for(char ch : s.toCharArray()){
+            
+            if(ch == '('){
+                count++;
+            }
+            else if(ch == ')'){
+                
+                if(count == 0) return false;
+                count--;
+            }
+        }
+        
+        return count == 0;
+    }
+}
