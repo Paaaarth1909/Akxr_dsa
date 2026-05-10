b84c56661af0af0718fbacea385199e27946ac13
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-10 14:28:33 +0530

    Completed

--- LEETCODE/MEDIUM/Non_overlapping_intervals_435.java
+++ LEETCODE/MEDIUM/Non_overlapping_intervals_435.java
@@ -28,3 +28,23 @@ intervals[i].length == 2
 -5 * 104 <= starti < endi <= 5 * 104
  
 */
+import java.util.*;
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]); 
+
+        int count = 0;
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < prevEnd) {
+                count++; 
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+}
