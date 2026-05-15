5a319fe7786653244f78014e67d45971a29399a3
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-15 11:55:15 +0530

    COMMIT

--- LEETCODE/EASY/MIn_cost_climbing_stairs_746.java
+++ LEETCODE/EASY/MIn_cost_climbing_stairs_746.java
@@ -32,3 +32,19 @@ Constraints:
 2 <= cost.length <= 1000
 0 <= cost[i] <= 999
 */
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+
+        for (int i = 2; i < n; i++) {
+            int curr = cost[i] + Math.min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return Math.min(prev1, prev2);
+    }
+}
