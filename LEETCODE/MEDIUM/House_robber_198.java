175d6170c2a968a15d31eb673f3c15b7e988305e
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-16 13:32:13 +0530

    COMMIT

--- LEETCODE/MEDIUM/House_robber_198.java
+++ LEETCODE/MEDIUM/House_robber_198.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
 
 Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
@@ -23,3 +25,18 @@ Constraints:
 1 <= nums.length <= 100
 0 <= nums[i] <= 400
 */
+class Solution {
+    public int rob(int[] nums) {
+        int prev2 = 0; 
+        int prev1 = 0; 
+
+        for (int num : nums) {
+            int curr = Math.max(prev1, prev2 + num);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
