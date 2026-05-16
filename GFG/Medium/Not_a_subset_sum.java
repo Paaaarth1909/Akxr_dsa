d25e04b66fbbf33d351dc3e73734d3c4441aeba1
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-16 13:24:46 +0530

    COMMIT

---
+++ GFG/Medium/Not_a_subset_sum.java
@@ -0,0 +1,14 @@
+/* Given a array arr[] of positive integers, find the smallest positive integer such that it cannot be represented as the sum of elements of any subset of the given array set.
+
+Examples:
+
+Input: arr[] = [3, 1, 2]
+Output: 7
+Explanation: 7 is the smallest positive number for which no subset is there with sum 7.
+Input: arr[] = [3, 10, 9, 6, 20, 28]
+Output: 1
+Explanation: 1 is the smallest positive number for which no subset is there with sum 1.
+Constraints
+1 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 103
+*/
