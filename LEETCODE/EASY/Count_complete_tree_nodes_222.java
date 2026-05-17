237a47d540174c4e2b08a6bc9e28f5e5b4e2beec
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-17 16:46:30 +0530

    COMMIT

--- LEETCODE/EASY/Count_complete_tree_nodes_222.java
+++ LEETCODE/EASY/Count_complete_tree_nodes_222.java
@@ -27,3 +27,55 @@ The number of nodes in the tree is in the range [0, 5 * 104].
 0 <= Node.val <= 5 * 104
 The tree is guaranteed to be complete.
 */
+
+  public class Count_complete_tree_nodes_222 {
+      int val;
+      Count_complete_tree_nodes_222 left;
+      Count_complete_tree_nodes_222 right;
+      Count_complete_tree_nodes_222() {}
+      Count_complete_tree_nodes_222(int val) { this.val = val; }
+      Count_complete_tree_nodes_222(int val, Count_complete_tree_nodes_222 left, Count_complete_tree_nodes_222 right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class Solution {
+    public int countNodes(Count_complete_tree_nodes_222 root) {
+        if (root == null) return 0;
+
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        // Perfect binary tree
+        if (leftHeight == rightHeight) {
+            return (1 << leftHeight) - 1;
+        }
+
+        // Otherwise recurse
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    private int getLeftHeight(Count_complete_tree_nodes_222 node) {
+        int height = 0;
+
+        while (node != null) {
+            height++;
+            node = node.left;
+        }
+
+        return height;
+    }
+
+    private int getRightHeight(Count_complete_tree_nodes_222 node) {
+        int height = 0;
+
+        while (node != null) {
+            height++;
+            node = node.right;
+        }
+
+        return height;
+    }
+}
