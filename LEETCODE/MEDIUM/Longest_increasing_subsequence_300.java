7b13a031b95b3d9295cdaa1e500be9ee376d0dd9
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-19 21:35:33 +0530

    COMMIT

--- LEETCODE/MEDIUM/Longest_increasing_subsequence_300.java
+++ LEETCODE/MEDIUM/Longest_increasing_subsequence_300.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* Given an integer array nums, return the length of the longest strictly increasing subsequence.
 
  
@@ -24,3 +26,29 @@ Constraints:
  
 
 */
+
+import java.util.Arrays;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, 1);
+
+        int maxLen = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+}
