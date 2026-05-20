67ed9eb70207fdd43a5c89947c30c67c7d8b11a7
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-20 13:45:26 +0530

    COMMIT

--- LEETCODE/MEDIUM/Partition_equal_subset_sum_416.java
+++ LEETCODE/MEDIUM/Partition_equal_subset_sum_416.java
@@ -19,3 +19,27 @@ Constraints:
 1 <= nums.length <= 200
 1 <= nums[i] <= 100
 */
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int total = 0;
+
+        for (int num : nums) {
+            total += num;
+        }
+
+        if (total % 2 != 0) return false;
+
+        int target = total / 2;
+
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
