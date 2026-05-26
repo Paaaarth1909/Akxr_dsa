bde3be71590384e7aa6095c51590e3112943f501
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-26 06:43:54 +0530

    COMMIT

--- LEETCODE/MEDIUM/Interleaving_string_97.java
+++ LEETCODE/MEDIUM/Interleaving_string_97.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
 
 An interleaving of two strings s and t is a configuration where s and t are divided into n and m substrings respectively, such that:
@@ -36,3 +38,37 @@ Constraints:
 0 <= s3.length <= 200
 s1, s2, and s3 consist of lowercase English letters.
 */
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        int m = s1.length();
+        int n = s2.length();
+
+        if (m + n != s3.length()) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true;
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+
+                if (i > 0 &&
+                    s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
+
+                    dp[i][j] |= dp[i - 1][j];
+                }
+
+                if (j > 0 &&
+                    s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
+
+                    dp[i][j] |= dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
