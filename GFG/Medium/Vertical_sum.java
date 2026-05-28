b00319e60611d6cb98d89a1a52d248758a1f79d7
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-28 14:06:47 +0530

    COMMIT

--- GFG/Medium/Vertical_sum.java
+++ GFG/Medium/Vertical_sum.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given a binary tree having n nodes, find the vertical sum of the nodes that are in the same vertical line. Return all sums through different vertical lines starting from the left-most vertical line to the right-most vertical line.
 
 Examples:
@@ -26,3 +28,61 @@ Constraints:
 1<= Node value <= 105
 
 */
+import java.util.*;
+
+class Node{
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+
+class Solution {
+
+    public ArrayList<Integer> verticalSum(Node root) {
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(root, 0));
+
+        while(!q.isEmpty()) {
+
+            Pair curr = q.poll();
+
+            Node node = curr.node;
+            int hd = curr.hd;
+
+            map.put(hd, map.getOrDefault(hd, 0) + node.data);
+
+            // left child
+            if(node.left != null) {
+                q.offer(new Pair(node.left, hd - 1));
+            }
+
+            // right child
+            if(node.right != null) {
+                q.offer(new Pair(node.right, hd + 1));
+            }
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    // helper class
+    class Pair {
+
+        Node node;
+        int hd;
+
+        Pair(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+}
