c8ac15da025bcc6a3754ba50e7deb1636a56a016
Parthsaarthie sharma <toshit113@gmail.com>
2026-05-29 13:19:53 +0530

    COMMIT

--- LEETCODE/MEDIUM/MIn_cost_to_connect_all_points_1584.java
+++ LEETCODE/MEDIUM/MIn_cost_to_connect_all_points_1584.java
@@ -27,3 +27,52 @@ Constraints:
 -106 <= xi, yi <= 106
 All pairs (xi, yi) are distinct.
 */
+import java.util.*;
+
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+
+        int n = points.length;
+
+        boolean[] visited = new boolean[n];
+
+        PriorityQueue<int[]> pq =
+            new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        pq.offer(new int[]{0, 0});
+
+        int totalCost = 0;
+        int edgesUsed = 0;
+
+        while (edgesUsed < n) {
+
+            int[] curr = pq.poll();
+
+            int cost = curr[0];
+            int node = curr[1];
+
+            if (visited[node]) {
+                continue;
+            }
+
+            visited[node] = true;
+
+            totalCost += cost;
+            edgesUsed++;
+
+            for (int next = 0; next < n; next++) {
+
+                if (!visited[next]) {
+
+                    int dist =
+                        Math.abs(points[node][0] - points[next][0]) +
+                        Math.abs(points[node][1] - points[next][1]);
+
+                    pq.offer(new int[]{dist, next});
+                }
+            }
+        }
+
+        return totalCost;
+    }
+}
