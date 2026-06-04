dec53a0c257c6d5d6201cab13abd9ee33a4b3845
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-04 08:07:38 +0530

    COMMIT

--- LEETCODE/MEDIUM/Unique_paths_63.java
+++ LEETCODE/MEDIUM/Unique_paths_63.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 
 An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
@@ -31,3 +33,38 @@ n == obstacleGrid[i].length
 1 <= m, n <= 100
 obstacleGrid[i][j] is 0 or 1.
 */
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        int[][] dp = new int[m][n];
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+
+                if (j > 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
