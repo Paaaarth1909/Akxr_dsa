cd91c923adcc4b642770720d3f2bb80a18c31dfe
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-05 22:47:42 +0530

    COMMIT

--- GFG/Medium/Lexicographically_smallest_after_removing_K.java
+++ GFG/Medium/Lexicographically_smallest_after_removing_K.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given a string s consisting of n lowercase characters. Return the lexicographically smallest string after removing exactly k characters from the string. But you have to correct the value of k, i.e., if the length of the string is a power of 2, reduce k by half, else multiply k by 2. You can remove any k characters.
 
 Note: If it is not possible to remove k (the value of k after correction) characters or if the resulting string is empty return -1.
@@ -15,3 +17,36 @@ Constraints:
 1 ≤ k ≤ 105
 
 */
+class Solution {
+    public String lexicographicallySmallest(String s, int k) {
+        int n = s.length();
+
+        if ((n & (n - 1)) == 0) { 
+            k /= 2;
+        } else {
+            k *= 2;
+        }
+
+        if (k >= n) {
+            return "-1";
+        }
+
+        StringBuilder stack = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            while (k > 0 && stack.length() > 0 &&
+                   stack.charAt(stack.length() - 1) > ch) {
+                stack.deleteCharAt(stack.length() - 1);
+                k--;
+            }
+            stack.append(ch);
+        }
+
+        while (k > 0 && stack.length() > 0) {
+            stack.deleteCharAt(stack.length() - 1);
+            k--;
+        }
+
+        return stack.length() == 0 ? "-1" : stack.toString();
+    }
+}
