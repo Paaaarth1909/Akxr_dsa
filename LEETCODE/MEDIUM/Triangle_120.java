c077de72e9a234b13b4d82346b0dfebee453b440
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-06 11:06:39 +0530

    COMMIT

--- LEETCODE/MEDIUM/Triangle_120.java
+++ LEETCODE/MEDIUM/Triangle_120.java
@@ -1,3 +1,7 @@
+package LEETCODE.MEDIUM;
+
+import java.util.List;
+
 /* Given a triangle array, return the minimum path sum from top to bottom.
 
 For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
@@ -27,3 +31,26 @@ triangle[0].length == 1
 triangle[i].length == triangle[i - 1].length + 1
 -104 <= triangle[i][j] <= 104
 */
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        int n = triangle.size();
+
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+
+        for (int row = n - 2; row >= 0; row--) {
+
+            for (int col = 0; col <= row; col++) {
+
+                dp[col] = triangle.get(row).get(col) +
+                          Math.min(dp[col], dp[col + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+}
