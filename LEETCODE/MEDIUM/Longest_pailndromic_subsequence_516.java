4345e83bb4f9e1a64e316f5f3afc7941fa202ebb
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-08 12:55:13 +0530

    COMMIT

--- LEETCODE/MEDIUM/Longest_pailndromic_subsequence_516.java
+++ LEETCODE/MEDIUM/Longest_pailndromic_subsequence_516.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* Given a string s, find the longest palindromic subsequence's length in s.
 
 A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
@@ -23,3 +25,41 @@ s consists only of lowercase English letters.
  
 
 */
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+
+        int n = s.length();
+
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int len = 2; len <= n; len++) {
+
+            for (int i = 0; i <= n - len; i++) {
+
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j)) {
+
+                    if (len == 2) {
+                        dp[i][j] = 2;
+                    } else {
+                        dp[i][j] = 2 + dp[i + 1][j - 1];
+                    }
+
+                } else {
+
+                    dp[i][j] = Math.max(
+                        dp[i + 1][j],
+                        dp[i][j - 1]
+                    );
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
