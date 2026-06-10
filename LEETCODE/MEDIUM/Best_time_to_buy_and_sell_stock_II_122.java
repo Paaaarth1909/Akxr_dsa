7fd058a5c8f9d6e15a3df6d87d8881082977587b
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-10 06:58:06 +0530

    COMMIT

--- LEETCODE/MEDIUM/Best_time_to_buy_and_sell_stock_II_122.java
+++ LEETCODE/MEDIUM/Best_time_to_buy_and_sell_stock_II_122.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 
 On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
@@ -31,3 +33,19 @@ Constraints:
 1 <= prices.length <= 3 * 104
 0 <= prices[i] <= 104
 */
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] > prices[i - 1]) {
+
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
