86e36124d17b2441397791a41e0128045d90f154
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-11 11:54:02 +0530

    COMMIT

--- GFG/Easy/Equal_point_in_brackets.java
+++ GFG/Easy/Equal_point_in_brackets.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given a string s of opening and closing brackets '(' and ')' only, find an equal point in the string. An equal point is a position k (0-based) such that the number of opening brackets before position k is equal to the number of closing brackets from position k to the end of the string. If multiple such points exist, return the first valid position.
 
 The string can be split at any position from 0 to n, where n is the length of the string.
@@ -15,3 +17,36 @@ Explanation: After index 2, the string splits into "))" and an empty string. The
 Constraints:
 1 ≤ s.size() ≤ 105 
 */
+class Solution {
+    public int findIndex(String s) {
+        
+        int n = s.length();
+        
+        int closeRight = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == ')') {
+                closeRight++;
+            }
+        }
+        
+        int openLeft = 0;
+        
+        for (int i = 0; i <= n; i++) {
+            
+            if (openLeft == closeRight) {
+                return i;
+            }
+            
+            if (i < n) {
+                if (s.charAt(i) == '(') {
+                    openLeft++;
+                } else {
+                    closeRight--;
+                }
+            }
+        }
+        
+        return n;
+    }
+}
