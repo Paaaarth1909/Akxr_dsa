df24288b7a43f1dbb26b3e5b96adb1e5e501b93d
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-15 07:40:24 +0530

    COMMIT

--- LEETCODE/MEDIUM/Zig_Zag_conversion_6.java
+++ LEETCODE/MEDIUM/Zig_Zag_conversion_6.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 
 P   A   H   N
@@ -35,3 +37,39 @@ Constraints:
 s consists of English letters (lower-case and upper-case), ',' and '.'.
 1 <= numRows <= 1000
 */
+class Solution {
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+
+            rows[currentRow].append(c);
+
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
