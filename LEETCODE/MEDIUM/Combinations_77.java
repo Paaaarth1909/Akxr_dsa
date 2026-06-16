6891388d9bcef0c2447fc295967482cf3f93c64f
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2026-06-16 19:09:15 +0530

    Update Combinations_77.java
    COMMIT

--- LEETCODE/MEDIUM/Combinations_77.java
+++ LEETCODE/MEDIUM/Combinations_77.java
@@ -22,3 +22,37 @@ Constraints:
 1 <= n <= 20
 1 <= k <= n
 */
+import java.util.*;
+
+class Solution {
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(1, n, k, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrack(int start,
+                           int n,
+                           int k,
+                           List<Integer> current,
+                           List<List<Integer>> result) {
+
+        if (current.size() == k) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+
+            current.add(i);
+
+            backtrack(i + 1, n, k, current, result);
+
+            current.remove(current.size() - 1);
+        }
+    }
+}
