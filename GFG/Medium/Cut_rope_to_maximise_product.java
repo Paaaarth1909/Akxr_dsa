b60f68fb7cb56f70692becb6e0c2cf4ae18ec3ac
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2026-06-17 17:17:14 +0530

    Update Cut_rope_to_maximise_product.java
    COMMIT

--- GFG/Medium/Cut_rope_to_maximise_product.java
+++ GFG/Medium/Cut_rope_to_maximise_product.java
@@ -11,3 +11,18 @@ Explanation: Maximum obtainable product is 2 * 3 = 6.
 Constraints:
 2 ≤ n ≤ 58
 */
+class Solution {
+    public int maxProduct(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        int product = 1;
+
+        while (n > 4) {
+            product *= 3;
+            n -= 3;
+        }
+
+        return product * n;
+    }
+}
