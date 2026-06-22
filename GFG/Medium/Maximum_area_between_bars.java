05d805ec7c1120ce9bb3b0c71095ace30729576f
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-22 11:11:38 +0530

    COMMIT

--- GFG/Medium/Maximum_area_between_bars.java
+++ GFG/Medium/Maximum_area_between_bars.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* class Solution {
     public int maxNumberOfBalloons(String text) {
 
@@ -17,3 +19,31 @@
     }
 } 
 */
+
+import java.util.List;
+
+class Solution {
+    public int maxArea(List<Integer> height) {
+        
+        int left = 0;
+        int right = height.size() - 1;
+        
+        int maxArea = 0;
+        
+        while (left < right) {
+            
+            int h = Math.min(height.get(left), height.get(right));
+            int width = right - left - 1;
+            
+            maxArea = Math.max(maxArea, h * width);
+            
+            if (height.get(left) < height.get(right)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
