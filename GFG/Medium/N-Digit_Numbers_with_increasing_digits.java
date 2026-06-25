3d9934a031b4419abbe68d9dfd606e771f06fd8c
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-25 11:44:01 +0530

    COMMIT

--- GFG/Medium/N-Digit_Numbers_with_increasing_digits.java
+++ GFG/Medium/N-Digit_Numbers_with_increasing_digits.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given an integer n, return all the n digit numbers in increasing order, such that their digits are in strictly increasing order(from left to right).
 
 Examples :
@@ -15,3 +17,37 @@ Constraints:
 1 ≤ n ≤ 105
 
 */
+import java.util.*;
+
+class Solution {
+
+    public static ArrayList<Integer> increasingNumbers(int n) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if (n > 10) return ans;
+
+        if (n == 1) {
+            for (int i = 0; i <= 9; i++) {
+                ans.add(i);
+            }
+            return ans;
+        }
+
+        generate(1, n, 0, ans);
+
+        return ans;
+    }
+
+    static void generate(int start, int n, int num, ArrayList<Integer> ans) {
+
+        if (n == 0) {
+            ans.add(num);
+            return;
+        }
+
+        for (int d = start; d <= 9; d++) {
+            generate(d + 1, n - 1, num * 10 + d, ans);
+        }
+    }
+}
