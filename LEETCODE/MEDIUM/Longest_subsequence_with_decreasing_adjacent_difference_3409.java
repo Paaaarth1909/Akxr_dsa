0c328524f2753cb9c5bca9d4a45049b2bf8d44a4
Parthsaarthie Sharma <parthsaarthiesharma1909@yahoo.com>
2026-06-26 13:00:05 +0530

    COMMIT

--- LEETCODE/MEDIUM/Longest_subsequence_with_decreasing_adjacent_difference_3409.java
+++ LEETCODE/MEDIUM/Longest_subsequence_with_decreasing_adjacent_difference_3409.java
@@ -43,3 +43,52 @@ Constraints:
 2 <= nums.length <= 104
 1 <= nums[i] <= 300
 */
+class Solution {
+    public int longestSubsequence(int[] nums) {
+
+        int MAXV = 300;
+        int MAXD = 299;
+
+        int[][] dp = new int[MAXV + 1][MAXD + 1];
+        int[][] suffix = new int[MAXV + 1][MAXD + 2];
+        boolean[] seen = new boolean[MAXV + 1];
+
+        int ans = 1;
+
+        for (int x : nums) {
+
+            int[] cur = new int[MAXD + 1];
+
+            for (int v = 1; v <= MAXV; v++) {
+
+                if (!seen[v]) continue;
+
+                int d = Math.abs(x - v);
+
+                int best = suffix[v][d];
+
+                if (best > 0) {
+                    cur[d] = Math.max(cur[d], best + 1);
+                } else {
+                    cur[d] = Math.max(cur[d], 2);
+                }
+            }
+
+            for (int d = 0; d <= MAXD; d++) {
+                if (cur[d] > dp[x][d]) {
+                    dp[x][d] = cur[d];
+                }
+                ans = Math.max(ans, dp[x][d]);
+            }
+
+            seen[x] = true;
+
+            suffix[x][MAXD] = dp[x][MAXD];
+            for (int d = MAXD - 1; d >= 0; d--) {
+                suffix[x][d] = Math.max(dp[x][d], suffix[x][d + 1]);
+            }
+        }
+
+        return ans;
+    }
+}
