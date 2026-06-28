2c9588d6bbafe660d6d118c6e834ec9e2647784d
Parthsaarthie sharma <toshit113@gmail.com>
2026-06-28 10:48:52 +0530

    COMMIT

--- GFG/Medium/K_Times_adjacent_appearing_two_1's.java
+++ GFG/Medium/K_Times_adjacent_appearing_two_1's.java
@@ -1,3 +1,5 @@
+package GFG.Medium;
+
 /* Given two integers n and k, count the number of binary strings of length n where adjacent 1 appear k times.
 
 Since the answer can be huge, return it modulo 109+7.
@@ -14,3 +16,30 @@ Constraints:
 1 ≤ n, k ≤ 103
 
 */
+class Solution {
+    public int countStrings(int n, int k) {
+        
+        int MOD = 1000000007;
+        
+        long[][][] dp = new long[n + 1][k + 1][2];
+        
+        dp[1][0][0] = 1;
+        dp[1][0][1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            
+            for (int j = 0; j <= k; j++) {
+                
+                dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+                
+                dp[i][j][1] = (dp[i][j][1] + dp[i - 1][j][0]) % MOD;
+                
+                if (j > 0) {
+                    dp[i][j][1] = (dp[i][j][1] + dp[i - 1][j - 1][1]) % MOD;
+                }
+            }
+        }
+        
+        return (int)((dp[n][k][0] + dp[n][k][1]) % MOD);
+    }
+}
