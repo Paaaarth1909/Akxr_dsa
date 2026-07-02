e76e289e0153eaf1e4ae7badf5d5c0f650a03c21
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-02 14:45:30 +0530

    COMMIT

--- LEETCODE/MEDIUM/Find_a_safe_walk_through_the_a_grid_3286.java
+++ LEETCODE/MEDIUM/Find_a_safe_walk_through_the_a_grid_3286.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* You are given an m x n binary matrix grid and an integer health.
 
 You start on the upper-left corner (0, 0) and would like to get to the lower-right corner (m - 1, n - 1).
@@ -57,3 +59,48 @@ n == grid[i].length
 1 <= health <= m + n
 grid[i][j] is either 0 or 1.
 */
+import java.util.*;
+
+class Solution {
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+
+        int m = grid.size(), n = grid.get(0).size();
+
+        int[][] dist = new int[m][n];
+        for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
+
+        Deque<int[]> dq = new ArrayDeque<>();
+
+        dist[0][0] = grid.get(0).get(0);
+        dq.offerFirst(new int[]{0, 0});
+
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!dq.isEmpty()) {
+
+            int[] cur = dq.pollFirst();
+            int x = cur[0], y = cur[1];
+
+            for (int[] d : dir) {
+
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+
+                int w = grid.get(nx).get(ny);
+                int nd = dist[x][y] + w;
+
+                if (nd < dist[nx][ny]) {
+
+                    dist[nx][ny] = nd;
+
+                    if (w == 0) dq.offerFirst(new int[]{nx, ny});
+                    else dq.offerLast(new int[]{nx, ny});
+                }
+            }
+        }
+
+        return dist[m - 1][n - 1] < health;
+    }
+}
