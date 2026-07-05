33dde5832a3f27b8bd1edc0094974fe85e3fbc19
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-05 12:23:35 +0530

    COMMIT

---
+++ GFG/Easy/Max_gap_between_two_same.java
@@ -0,0 +1,14 @@
+/* Given a string s consisting of lowercase English letters, find the maximum number of characters between any two identical characters. If no character repeats, return -1.
+
+Examples :
+
+Input: s = "socks"
+Output: 3
+Explanation: There are 3 characters between the two occurrences of 's'.
+Input: s = "for"
+Output: -1
+Explanation: No repeating character present.
+Constraints:
+1 ≤ |s| ≤ 105
+
+*/
