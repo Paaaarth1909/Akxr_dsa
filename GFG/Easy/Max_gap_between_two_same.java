f274419d6d2e97819f1d4c60c9248793676391ac
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-05 12:23:55 +0530

    COMMIT

--- GFG/Easy/Max_gap_between_two_same.java
+++ GFG/Easy/Max_gap_between_two_same.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given a string s consisting of lowercase English letters, find the maximum number of characters between any two identical characters. If no character repeats, return -1.
 
 Examples :
@@ -12,3 +14,28 @@ Constraints:
 1 ≤ |s| ≤ 105
 
 */
+class Solution {
+    public int maxCharGap(String s) {
+        
+        int[] first = new int[26];
+        
+        for (int i = 0; i < 26; i++) {
+            first[i] = -1;
+        }
+        
+        int ans = -1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            int idx = s.charAt(i) - 'a';
+            
+            if (first[idx] == -1) {
+                first[idx] = i;
+            } else {
+                ans = Math.max(ans, i - first[idx] - 1);
+            }
+        }
+        
+        return ans;
+    }
+}
