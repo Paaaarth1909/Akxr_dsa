4b53c3d32ed055696dbfe6288c12dd4ea1621eee
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-05 12:25:32 +0530

    COMMIT

--- LEETCODE/MEDIUM/Longest_string_chain_1048.java
+++ LEETCODE/MEDIUM/Longest_string_chain_1048.java
@@ -1,3 +1,5 @@
+package LEETCODE.MEDIUM;
+
 /* You are given an array of words where each word consists of lowercase English letters.
 
 wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without changing the order of the other characters to make it equal to wordB.
@@ -33,3 +35,31 @@ Constraints:
 1 <= words[i].length <= 16
 words[i] only consists of lowercase English letters.
 */
+import java.util.*;
+
+class Solution {
+    public int longestStrChain(String[] words) {
+
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        Map<String, Integer> dp = new HashMap<>();
+        int ans = 1;
+
+        for (String word : words) {
+
+            int best = 1;
+
+            for (int i = 0; i < word.length(); i++) {
+
+                String prev = word.substring(0, i) + word.substring(i + 1);
+
+                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+            }
+
+            dp.put(word, best);
+            ans = Math.max(ans, best);
+        }
+
+        return ans;
+    }
+}
