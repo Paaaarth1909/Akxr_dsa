d463aace15e7a836855dd1c99d8d42d3c0cbfe7a
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-14 14:58:34 +0530

    COMMIT

--- LEETCODE/HARD/Subarrays_with_K_different_integers_992.java
+++ LEETCODE/HARD/Subarrays_with_K_different_integers_992.java
@@ -1,3 +1,4 @@
+package LEETCODE.HARD;
 /* Given an integer array nums and an integer k, return the number of good subarrays of nums.
 
 A good array is an array where the number of different integers in that array is exactly k.
@@ -24,3 +25,39 @@ Constraints:
 1 <= nums.length <= 2 * 104
 1 <= nums[i], k <= nums.length
 */
+import java.util.*;
+
+class Solution {
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+
+            while (map.size() > k) {
+
+                map.put(nums[left], map.get(nums[left]) - 1);
+
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+
+                left++;
+            }
+
+            ans += right - left + 1;
+        }
+
+        return ans;
+    }
+}
