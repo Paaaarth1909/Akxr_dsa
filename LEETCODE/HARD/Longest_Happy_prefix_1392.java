804ea01f220ba0c0ba8aaaae5d35e2c6d346a819
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-16 22:31:05 +0530

    COMMIT

--- LEETCODE/HARD/Longest_Happy_prefix_1392.java
+++ LEETCODE/HARD/Longest_Happy_prefix_1392.java
@@ -1,3 +1,6 @@
+
+package LEETCODE.HARD;
+
 /* A string is called a happy prefix if is a non-empty prefix which is also a suffix (excluding itself).
 
 Given a string s, return the longest happy prefix of s. Return an empty string "" if no such prefix exists.
@@ -22,3 +25,32 @@ Constraints:
 s contains only lowercase English letters.
  
 */
+class Solution {
+    public String longestPrefix(String s) {
+
+        int n = s.length();
+        int[] lps = new int[n];
+
+        int len = 0;
+        int i = 1;
+
+        while (i < n) {
+
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return s.substring(0, lps[n - 1]);
+    }
+}
