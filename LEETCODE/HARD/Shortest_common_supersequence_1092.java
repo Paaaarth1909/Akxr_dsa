dff84fd1d3e5a8512e5b420909900236ef6a1345
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-24 19:12:01 +0530

    COMMIT

--- LEETCODE/HARD/Shortest_common_supersequence_1092.java
+++ LEETCODE/HARD/Shortest_common_supersequence_1092.java
@@ -1,3 +1,4 @@
+package LEETCODE.HARD;
 /* Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences. If there are multiple valid strings, return any of them.
 
 A string s is a subsequence of string t if deleting some number of characters from t (possibly 0) results in the string s.
@@ -23,3 +24,52 @@ Constraints:
 1 <= str1.length, str2.length <= 1000
 str1 and str2 consist of lowercase English letters.
 */
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+
+        int n = str1.length();
+        int m = str2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        int i = n, j = m;
+
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                ans.append(str1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] >= dp[i][j - 1]) {
+                ans.append(str1.charAt(i - 1));
+                i--;
+            } else {
+                ans.append(str2.charAt(j - 1));
+                j--;
+            }
+        }
+
+        while (i > 0) {
+            ans.append(str1.charAt(i - 1));
+            i--;
+        }
+
+        while (j > 0) {
+            ans.append(str2.charAt(j - 1));
+            j--;
+        }
+
+        return ans.reverse().toString();
+    }
+}
