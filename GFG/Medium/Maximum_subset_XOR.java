588a41554c018597eb5d3a9d94174eb72078b2dd
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-30 18:20:35 +0530

    COMMIT

---
+++ GFG/Medium/Maximum_subset_XOR.java
@@ -0,0 +1,15 @@
+/* Given an array arr[], choose any subset of elements (possibly all elements) such that the XOR of the chosen elements is maximized.
+
+Examples:
+
+Input : arr[] = [2, 4, 5]
+Output: 7
+Explanation: The subset {2, 5} has the maximum XOR value.
+Input : arr[] = [9, 8, 5]
+Output: 13
+Explanation: The subset {8, 5} has the maximum XOR value.
+Constraints:
+
+1 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 106
+*/
