2069f8af596178a5650c03209e04d9c6064999e4
Parthsaarthie sharma <toshit113@gmail.com>
2026-07-31 21:11:18 +0530

    COMMIT

--- LEETCODE/MEDIUM/Implement_trie_208.java
+++ LEETCODE/MEDIUM/Implement_trie_208.java
@@ -32,3 +32,67 @@ Constraints:
 word and prefix consist only of lowercase English letters.
 At most 3 * 104 calls in total will be made to insert, search, and startsWith.
 */
+class Trie {
+
+    class Node {
+        Node[] child = new Node[26];
+        boolean end;
+    }
+
+    Node root;
+
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+
+        Node cur = root;
+
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+
+            if (cur.child[i] == null) {
+                cur.child[i] = new Node();
+            }
+
+            cur = cur.child[i];
+        }
+
+        cur.end = true;
+    }
+
+    public boolean search(String word) {
+
+        Node cur = root;
+
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+
+            if (cur.child[i] == null) {
+                return false;
+            }
+
+            cur = cur.child[i];
+        }
+
+        return cur.end;
+    }
+
+    public boolean startsWith(String prefix) {
+
+        Node cur = root;
+
+        for (char c : prefix.toCharArray()) {
+            int i = c - 'a';
+
+            if (cur.child[i] == null) {
+                return false;
+            }
+
+            cur = cur.child[i];
+        }
+
+        return true;
+    }
+}
