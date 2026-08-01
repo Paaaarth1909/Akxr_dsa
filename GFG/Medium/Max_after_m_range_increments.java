56b26d5b4a888ffcf101313c57eb1e67e24e5069
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-01 22:07:03 +0530

    COMMIT

--- GFG/Medium/Max_after_m_range_increments.java
+++ GFG/Medium/Max_after_m_range_increments.java
@@ -24,4 +24,26 @@ Constraints:
 0 ≤  ai  ≤  bi  ≤ n-1
 0 ≤  ki  ≤ 106 
 */
- 
+class Solution {
+    public int findMax(int n, int[] a, int[] b, int[] k) {
+
+        long[] diff = new long[n + 1];
+
+        for (int i = 0; i < a.length; i++) {
+            diff[a[i]] += k[i];
+            if (b[i] + 1 < n) {
+                diff[b[i] + 1] -= k[i];
+            }
+        }
+
+        long sum = 0;
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += diff[i];
+            ans = Math.max(ans, sum);
+        }
+
+        return (int) ans;
+    }
+}
