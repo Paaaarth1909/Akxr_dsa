035de6d9bf055005ab9908eb87eb87ec9f165c0b
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-03 11:45:27 +0530

    COMMIT

--- LEETCODE/HARD/Word_search_II_212.java
+++ LEETCODE/HARD/Word_search_II_212.java
@@ -1,3 +1,4 @@
+package LEETCODE.HARD;
 /* Given an m x n board of characters and a list of strings words, return all words on the board.
 
 Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
@@ -27,3 +28,71 @@ board[i][j] is a lowercase English letter.
 words[i] consists of lowercase English letters.
 All the strings of words are unique.
 */
+import java.util.*;
+
+class Solution {
+
+    class TrieNode {
+        TrieNode[] child = new TrieNode[26];
+        String word;
+    }
+
+    List<String> ans = new ArrayList<>();
+    int m, n;
+
+    public List<String> findWords(char[][] board, String[] words) {
+
+        TrieNode root = new TrieNode();
+
+        for (String word : words) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                int idx = c - 'a';
+                if (node.child[idx] == null) {
+                    node.child[idx] = new TrieNode();
+                }
+                node = node.child[idx];
+            }
+            node.word = word;
+        }
+
+        m = board.length;
+        n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, i, j, root);
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode node) {
+
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == '#') {
+            return;
+        }
+
+        char c = board[i][j];
+        node = node.child[c - 'a'];
+
+        if (node == null) {
+            return;
+        }
+
+        if (node.word != null) {
+            ans.add(node.word);
+            node.word = null;
+        }
+
+        board[i][j] = '#';
+
+        dfs(board, i + 1, j, node);
+        dfs(board, i - 1, j, node);
+        dfs(board, i, j + 1, node);
+        dfs(board, i, j - 1, node);
+
+        board[i][j] = c;
+    }
+}
