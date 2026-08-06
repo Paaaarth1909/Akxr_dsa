31d6adab4426b4af66ccf469c9e16de93363c4c5
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-06 23:11:48 +0530

    COMMIT

--- LEETCODE/EASY/Smallest_divisible_digital_product_I_3345.java
+++ LEETCODE/EASY/Smallest_divisible_digital_product_I_3345.java
@@ -29,3 +29,24 @@ Constraints:
 1 <= n <= 100
 1 <= t <= 10
 */
+class Solution {
+    public int smallestNumber(int n, int t) {
+
+        while (true) {
+
+            int x = n;
+            int prod = 1;
+
+            while (x > 0) {
+                prod *= x % 10;
+                x /= 10;
+            }
+
+            if (prod % t == 0) {
+                return n;
+            }
+
+            n++;
+        }
+    }
+}
