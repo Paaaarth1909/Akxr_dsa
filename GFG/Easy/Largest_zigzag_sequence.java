d99d32bb9c95124066d655bd287be67298ff5a62
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-09 21:36:03 +0530

    COMMIT

--- GFG/Easy/Largest_zigzag_sequence.java
+++ GFG/Easy/Largest_zigzag_sequence.java
@@ -1,3 +1,4 @@
+package GFG.Easy;
 /* Given a square matrix mat[][] of size n × n. A zigzag sequence starts from the top and ends at the bottom. Two consecutive elements of sequence cannot belong to the same column.
 
 Return the maximum sum of such a zigzag sequence.
@@ -19,3 +20,47 @@ Constraints:
 1 ≤ mat[i][j] ≤ 1000 
 
 */
+class Solution {
+    public int zigzagSequence(int[][] mat) {
+
+        int n = mat.length;
+        int[] dp = new int[n];
+
+        for (int j = 0; j < n; j++) {
+            dp[j] = mat[0][j];
+        }
+
+        for (int i = 1; i < n; i++) {
+            int[] next = new int[n];
+
+            int max1 = -1;
+            int max2 = -1;
+            int idx = -1;
+
+            for (int j = 0; j < n; j++) {
+                if (dp[j] > max1) {
+                    max2 = max1;
+                    max1 = dp[j];
+                    idx = j;
+                } else if (dp[j] > max2) {
+                    max2 = dp[j];
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                int best = (j == idx) ? max2 : max1;
+                next[j] = mat[i][j] + best;
+            }
+
+            dp = next;
+        }
+
+        int ans = 0;
+
+        for (int x : dp) {
+            ans = Math.max(ans, x);
+        }
+
+        return ans;
+    }
+}
