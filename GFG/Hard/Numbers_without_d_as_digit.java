d720dd3ed06666b8cf8585812943623a56282037
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-15 15:03:42 +0530

    COMMIT

--- GFG/Hard/Numbers_without_d_as_digit.java
+++ GFG/Hard/Numbers_without_d_as_digit.java
@@ -1,1 +1,15 @@
-/*  */
+/* Given a number n, count the numbers from 1 to n that don’t contain digit d in their decimal representation.
+
+Examples:
+
+Input: n = 25, d = 3
+Output: 22
+Explanation: From 1 to 25, the numbers 3, 13, and 23 contain the digit 3, so the answer is 25 - 3 = 22.
+Input: n = 5, d = 3
+Output: 4
+Explanation: From 1 to 5, only 3 contains the digit 3, so the count of numbers without digit 3 is 4.
+Constraints:
+
+0 ≤ n ≤ 109
+0 ≤ d ≤ 9
+*/
