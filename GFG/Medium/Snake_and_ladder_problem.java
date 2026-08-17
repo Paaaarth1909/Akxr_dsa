0fa5034be559e136944dad718abef0fed830f78f
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-17 19:38:57 +0530

    COMMIT

--- GFG/Medium/Snake_and_ladder_problem.java
+++ GFG/Medium/Snake_and_ladder_problem.java
@@ -1,3 +1,4 @@
+package GFG.Medium;
 /* Given an integer n such that there is n × n Snakes and Ladders board with cells numbered from 1 to n*n, find the minimum number of dice throws required to reach cell n*n starting from cell 1. Given two arrays of even lengths:
 
 lad[], where each pair (lad[2*i], lad[2*i + 1]) represents the start and end of a ladder.
@@ -30,3 +31,64 @@ Constraints:
 1 ≤ n ≤ 103
 1 ≤ lad.size(), sn.size(), lad[i], sn[i] ≤ n2 
 */
+import java.util.*;
+
+ class Solution {
+     public int minThrows(int n, int[] lad, int[] sn) {
+
+         int size = n * n;
+         int[] move = new int[size + 1];
+
+         for (int i = 0; i < lad.length; i += 2) {
+             move[lad[i]] = lad[i + 1];
+         }
+
+         for (int i = 0; i < sn.length; i += 2) {
+             move[sn[i]] = sn[i + 1];
+         }
+
+         boolean[] vis = new boolean[size + 1];
+         Queue<Integer> q = new LinkedList<>();
+
+         q.offer(1);
+         vis[1] = true;
+
+         int throwsCount = 0;
+
+         while (!q.isEmpty()) {
+
+             int sz = q.size();
+
+             while (sz-- > 0) {
+
+                 int cur = q.poll();
+
+                 if (cur == size) {
+                     return throwsCount;
+                 }
+
+                 for (int dice = 1; dice <= 6; dice++) {
+
+                     int next = cur + dice;
+
+                     if (next > size) {
+                         continue;
+                     }
+
+                     if (move[next] != 0) {
+                         next = move[next];
+                     }
+
+                     if (!vis[next]) {
+                         vis[next] = true;
+                         q.offer(next);
+                     }
+                 }
+             }
+
+             throwsCount++;
+         }
+
+         return -1;
+     }
+ }
