54a54e73e4be9099bd32119608bde96c3765ac5a
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-18 15:31:07 +0530

    COMMIT

--- GFG/Hard/Secret_cipher.java
+++ GFG/Hard/Secret_cipher.java
@@ -21,3 +21,57 @@ Constraints:
 1 ≤ |s| ≤ 105
 
 */
+
+class Solution {
+    public String compress(String s) {
+
+        int n = s.length();
+
+        int[] z = new int[n];
+        int l = 0, r = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i <= r) {
+                z[i] = Math.min(r - i + 1, z[i - l]);
+            }
+
+            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i])) {
+                z[i]++;
+            }
+
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+
+        String[] dp = new String[n + 1];
+        int[] len = new int[n + 1];
+
+        dp[0] = "";
+        len[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            dp[i] = dp[i - 1] + s.charAt(i - 1);
+            len[i] = len[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                int half = i / 2;
+
+                if (z[half] >= half) {
+                    String candidate = dp[half] + "*";
+
+                    if (candidate.length() < dp[i].length()
+                            || (candidate.length() == dp[i].length()
+                            && candidate.compareTo(dp[i]) < 0)) {
+                        dp[i] = candidate;
+                        len[i] = candidate.length();
+                    }
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
