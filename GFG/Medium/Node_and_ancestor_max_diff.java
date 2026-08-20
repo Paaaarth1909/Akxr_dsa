d2116c4e6afb95440e2ab42f290c3a9cb73e428d
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-20 23:20:28 +0530

    COMMIT

--- GFG/Medium/Node_and_ancestor_max_diff.java
+++ GFG/Medium/Node_and_ancestor_max_diff.java
@@ -1,1 +1,18 @@
-/*  */
+/* Given the root of a binary tree, find the maximum difference between an ancestor node A and its descendant node B, i.e., maximize A - B.
+
+Examples :
+
+Input: root[] = [5, 2, 1] 
+
+Output: 4
+Explanation: The maximum difference we can get is 4, which is between 5 and 1.
+Input: root[] = [1, 2, 3, N, N, N, 7] 
+
+Output: -1
+Explanation: The maximum difference we can get is -1, which is between 1 and 2.
+Constraints:
+
+2 ≤ no. of nodes in root ≤ 104
+0 ≤ root.node->data ≤ 105
+2 ≤ Number of edges ≤ 104
+*/
