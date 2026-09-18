1b26f0c5edf5e7437218dc6f60d75670e3fea61f
Parthsaarthie sharma <toshit113@gmail.com>
2026-09-18 14:58:08 +0530

    Commit

--- LEETCODE/HARD/Maximum_number_of_non_overlapping_substrings_1520.java
+++ LEETCODE/HARD/Maximum_number_of_non_overlapping_substrings_1520.java
@@ -34,3 +34,61 @@ Constraints:
 1 <= s.length <= 105
 s contains only lowercase English letters.
 */
+import java.util.*;
+
+class Solution {
+
+    public List<String> maxNumOfSubstrings(String s) {
+
+        int n = s.length();
+        int[] first = new int[26];
+        int[] last = new int[26];
+
+        Arrays.fill(first, n);
+        Arrays.fill(last, -1);
+
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            first[c] = Math.min(first[c], i);
+            last[c] = i;
+        }
+
+        List<int[]> list = new ArrayList<>();
+
+        for (int c = 0; c < 26; c++) {
+
+            if (last[c] == -1) continue;
+
+            int l = first[c];
+            int r = last[c];
+            boolean ok = true;
+
+            for (int i = l; i <= r && ok; i++) {
+                int x = s.charAt(i) - 'a';
+                if (first[x] < l) {
+                    ok = false;
+                    break;
+                }
+                r = Math.max(r, last[x]);
+            }
+
+            if (ok) {
+                list.add(new int[]{l, r});
+            }
+        }
+
+        Collections.sort(list, (a, b) -> a[1] - b[1]);
+
+        List<String> ans = new ArrayList<>();
+        int end = -1;
+
+        for (int[] cur : list) {
+            if (cur[0] > end) {
+                ans.add(s.substring(cur[0], cur[1] + 1));
+                end = cur[1];
+            }
+        }
+
+        return ans;
+    }
+}
